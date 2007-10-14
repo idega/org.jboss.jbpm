b59153fef27b87118ed30615dfcc9d10bed1e247
Vytautas Civilis <civilis@idega.com>
2007-10-14 10:55:08 +0000

    removed method getIdentifier

--- src/java/com/idega/jbpm/def/ViewToTask.java
+++ src/java/com/idega/jbpm/def/ViewToTask.java
@@ -4,13 +4,12 @@ import org.jbpm.taskmgmt.def.Task;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  *
- * Last modified: $Date: 2007/09/18 09:45:39 $ by $Author: civilis $
+ * Last modified: $Date: 2007/10/14 10:55:08 $ by $Author: civilis $
  */
 public interface ViewToTask {
 
 	public abstract void bind(View view, Task task);
 	public abstract View getView(long taskId);
-	public abstract String getIdentifier();
 }
