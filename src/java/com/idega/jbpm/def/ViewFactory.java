3182a824c23fb39c3709746f3b3df6b2736e6471
Vytautas Civilis <civilis@idega.com>
2007-12-06 20:32:29 +0000

    *** empty log message ***

--- src/java/com/idega/jbpm/def/ViewFactory.java
+++ src/java/com/idega/jbpm/def/ViewFactory.java
@@ -2,15 +2,15 @@ package com.idega.jbpm.def;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  *
- * Last modified: $Date: 2007/12/04 18:49:48 $ by $Author: civilis $
+ * Last modified: $Date: 2007/12/06 20:32:29 $ by $Author: civilis $
  */
 public interface ViewFactory {
 
-	public abstract View createView();
-	
 	public abstract String getViewType();
+	
+	public abstract View getViewNoLoad(String viewIdentifier);
 	
 	public abstract View getView(String viewIdentifier, boolean submitable);
 }
