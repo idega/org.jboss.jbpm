8d4d8e99f61272bfac46d1d3ed243865b2fd1ae8
Vytautas Civilis <civilis@idega.com>
2008-06-12 18:28:10 +0000

    constants added

--- src/java/com/idega/jbpm/identity/BPMUser.java
+++ src/java/com/idega/jbpm/identity/BPMUser.java
@@ -10,13 +10,18 @@ import com.idega.user.data.User;
  *   
  *   
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  * 
- * Last modified: $Date: 2008/05/25 14:58:40 $ by $Author: civilis $
+ * Last modified: $Date: 2008/06/12 18:28:10 $ by $Author: civilis $
  */
 public interface BPMUser {
 	
-	public static final String bpmUserIdentifier = "BPM_USER";
+	public static final String bpmUsrParam = "bpmusr";
+	public static final String USER_TYPE_NATURAL = "BPM_USER_NATURAL";
+	public static final String USER_TYPE_LEGAL = "BPM_USER_LEGAL";
+	public static final String USER_TYPE = "BPM_USER_TYPE";
+	public static final String USER_ROLE = "BPM_USER_ROLE";
+	public static final String PROCESS_INSTANCE_ID = "BPM_PROCESS_INSTANCE_ID";
 
 	public abstract User getBpmUser();
 
