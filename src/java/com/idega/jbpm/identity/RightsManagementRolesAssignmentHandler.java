a2a79e7dfc8b02ec4f62b7db78fd47e86923d376
Anton Makarov <anton@idega.com>
2008-06-27 08:28:21 +0000

    removed unnecessary code

--- src/java/com/idega/jbpm/identity/RightsManagementRolesAssignmentHandler.java
+++ src/java/com/idega/jbpm/identity/RightsManagementRolesAssignmentHandler.java
@@ -2,20 +2,17 @@ package com.idega.jbpm.identity;
 
 import java.util.List;
 
-import javax.faces.context.FacesContext;
-
 import org.jbpm.graph.def.ActionHandler;
 import org.jbpm.graph.exe.ExecutionContext;
 
-import com.idega.presentation.IWContext;
 import com.idega.util.expression.ELUtil;
 
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  *
- * Last modified: $Date: 2008/06/26 15:33:33 $ by $Author: anton $
+ * Last modified: $Date: 2008/06/27 08:28:21 $ by $Author: anton $
  */
 public class RightsManagementRolesAssignmentHandler implements ActionHandler {
 
@@ -25,13 +22,8 @@ public class RightsManagementRolesAssignmentHandler implements ActionHandler {
 	private String assignmentExpression;
 	
 	public void execute(ExecutionContext ctx) throws Exception {
-		System.out.println(getAssignmentExpression());
-
 		if(getAssignmentExpression() != null) {
 			List<Role> roles = JSONExpHandler.resolveRightsRolesFromJSONExpression(getAssignmentExpression());
-			
-			FacesContext fctx = FacesContext.getCurrentInstance();
-			final IWContext iwc = IWContext.getIWContext(fctx);
 			
 			RolesAssiger rolesAssigner = getRolesAssigner();
 			rolesAssigner.assign(ctx.getProcessInstance(), roles);
