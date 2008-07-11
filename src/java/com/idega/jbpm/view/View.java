3099ae92847af0781b9886c1dc4cb7df9c8f910a
Anton Makarov <anton@idega.com>
2008-07-11 14:17:39 +0000

    added getDefaultDisplayName() method

--- src/java/com/idega/jbpm/view/View.java
+++ src/java/com/idega/jbpm/view/View.java
@@ -8,9 +8,9 @@ import javax.faces.component.UIComponent;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  *
- * Last modified: $Date: 2008/06/13 08:13:41 $ by $Author: anton $
+ * Last modified: $Date: 2008/07/11 14:17:39 $ by $Author: anton $
  */
 public interface View {
 
@@ -22,6 +22,7 @@ public interface View {
 	
 	public abstract String getDisplayName();
 	public abstract String getDisplayName(Locale locale);
+	public abstract String getDefaultDisplayName();
 	
 	public abstract void setViewType(String viewType);
 	public abstract String getViewType();
