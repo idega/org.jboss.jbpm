7dd9c6bab816f1b77937cbc7fc9ef002197eb573
Vytautas Civilis <civilis@idega.com>
2008-07-19 20:41:37 +0000

    upload process bundle as zip support added

--- src/java/com/idega/jbpm/presentation/beans/DeployProcess.java
+++ src/java/com/idega/jbpm/presentation/beans/DeployProcess.java
@@ -4,64 +4,100 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.zip.ZipInputStream;
 
 import org.apache.myfaces.custom.fileupload.UploadedFile;
-import org.jbpm.JbpmContext;
-import org.jbpm.graph.def.ProcessDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
 
-import com.idega.jbpm.BPMContext;
+import com.idega.core.file.util.MimeTypeUtil;
+import com.idega.idegaweb.IWMainApplication;
+import com.idega.jbpm.bundle.ProcessBundle;
+import com.idega.jbpm.bundle.ProcessBundleFactory;
+import com.idega.jbpm.bundle.ProcessBundleManager;
+import com.idega.jbpm.bundle.ProcessBundleResources;
+import com.idega.jbpm.bundle.ZippedBundleResourcesImpl;
+import com.idega.presentation.IWContext;
 
 /**
  * 
  * @author <a href="civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.6 $
+ * @version $Revision: 1.7 $
  *
- * Last modified: $Date: 2008/06/15 15:58:50 $ by $Author: civilis $
+ * Last modified: $Date: 2008/07/19 20:41:37 $ by $Author: civilis $
  *
  */
+@Scope("request")
+@Service("bpmDeployProcess")
 public class DeployProcess {
 	
-	private UploadedFile pd;
-	private BPMContext idegaJbpmContext;
+	private UploadedFile processBundle;
+	@Autowired
+	private ProcessBundleFactory processBundleFactory;
+	@Autowired
+	private ProcessBundleManager processBundleManager;
+	@Autowired
+	private ProcessBundleResources processBundleResources;
 	
-	public UploadedFile getProcessDefinition() {
-		return pd;
-	}
-	
-	public void setProcessDefinition(UploadedFile pd) {
-		this.pd = pd;
-	}
-	
-	public void upload() {
+	public void deploy() {
 		
-		JbpmContext ctx = getIdegaJbpmContext().createJbpmContext();
+		UploadedFile processBundle = getProcessBundle();
 		
-		InputStream is = null;
+		if(processBundle  != null && MimeTypeUtil.MIME_TYPE_ZIP.equals(processBundle.getContentType())) {
 		
-		try {
-			is = getProcessDefinition().getInputStream();
-			ctx.deployProcessDefinition(ProcessDefinition.parseXmlInputStream(is));
+			InputStream is = null;
+			
+			try {
+				is = getProcessBundle().getInputStream();
+				IWMainApplication iwma = IWContext.getCurrentInstance().getIWMainApplication();
+				
+				ZipInputStream zis = new ZipInputStream(is);
+				
+				ZippedBundleResourcesImpl resources = (ZippedBundleResourcesImpl)getProcessBundleResources();
+				resources.load(zis);
+				
+				ProcessBundle pb = getProcessBundleFactory().createProcessBundle(resources);
+				getProcessBundleManager().createBundle(pb, iwma);
+				System.out.println("__deployed");
 
-		} catch (IOException e) {
-			
-			Logger.getLogger(DeployProcess.class.getName()).log(Level.WARNING, "Exception while reading process while getting process definition input stream", e);
-//			TODO: display err msg
-			
-		} catch (Exception e) {
-			
-			Logger.getLogger(DeployProcess.class.getName()).log(Level.WARNING, "Exception while deploying process definition", e);
-//			TODO: display err msg				
-			
-		} finally {
-			getIdegaJbpmContext().closeAndCommit(ctx);
+			} catch (IOException e) {
+				
+				Logger.getLogger(DeployProcess.class.getName()).log(Level.WARNING, "Exception while reading process while getting process definition input stream", e);
+//				TODO: display err msg
+				
+			} catch (Exception e) {
+				
+				Logger.getLogger(DeployProcess.class.getName()).log(Level.WARNING, "Exception while deploying process definition", e);
+//				TODO: display err msg				
+				
+			} finally {
+				
+				try { 
+					if(is != null)
+						is.close();
+				} catch (IOException e) { }
+			}
 		}
 	}
 
-	public BPMContext getIdegaJbpmContext() {
-		return idegaJbpmContext;
+	public UploadedFile getProcessBundle() {
+		return processBundle;
 	}
 
-	public void setIdegaJbpmContext(BPMContext idegaJbpmContext) {
-		this.idegaJbpmContext = idegaJbpmContext;
+	public void setProcessBundle(UploadedFile processBundle) {
+		this.processBundle = processBundle;
+	}
+	
+	public ProcessBundleFactory getProcessBundleFactory() {
+		return processBundleFactory;
+	}
+	
+	public ProcessBundleManager getProcessBundleManager() {
+		return processBundleManager;
+	}
+
+	public ProcessBundleResources getProcessBundleResources() {
+		return processBundleResources;
 	}
 }
