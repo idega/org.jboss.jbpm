0493fbe68d9c7e086b1e1ec86d15fe6693cffe99
Vytautas Civilis <civilis@idega.com>
2008-08-25 19:04:35 +0000

    seeContacts added

--- src/java/com/idega/jbpm/identity/permission/Access.java
+++ src/java/com/idega/jbpm/identity/permission/Access.java
@@ -1,12 +1,25 @@
 package com.idega.jbpm.identity.permission;
 
 /**
+ * 
+ * <p>
+ * <b>
+ * about contactsCanBeSeen, seeContacts accesses </b> (those two are different):
+ * <p>
+ * contactsCanBeSeen means, that the role's contacts for which this access is specified, can be seen by the relevant subject (e.g. current user)
+ * </p>
+ * <p>
+ * seeContacts - the current role can see contacts (if has this access) of the roles specified. otherwise (if the seeContacts access is not specified),
+ * role is considered to be _not_ able to see those specified contacts
+ * </p>
+ * </p>
+ * 
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  *
- * Last modified: $Date: 2008/08/05 07:22:52 $ by $Author: civilis $
+ * Last modified: $Date: 2008/08/25 19:04:35 $ by $Author: civilis $
  */
 public enum Access {
 
-	read, write, modifyPermissions, caseHandler
+	read, write, modifyPermissions, caseHandler, contactsCanBeSeen, seeContacts
 }
