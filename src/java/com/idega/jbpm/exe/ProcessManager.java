ece01fd6f67dddbca4583128dd1080268b69cfcf
Vytautas Civilis <civilis@idega.com>
2008-09-17 18:17:32 +0000

    getProcessDefinition(String processName) added

--- src/java/com/idega/jbpm/exe/ProcessManager.java
+++ src/java/com/idega/jbpm/exe/ProcessManager.java
@@ -2,13 +2,19 @@ package com.idega.jbpm.exe;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.6 $
+ * @version $Revision: 1.7 $
  *
- * Last modified: $Date: 2008/09/02 12:59:47 $ by $Author: civilis $
+ * Last modified: $Date: 2008/09/17 18:17:32 $ by $Author: civilis $
  */
 public interface ProcessManager {
 	
 	public abstract ProcessDefinitionW getProcessDefinition(long pdId);
+	
+	/**
+	 * @param processName
+	 * @return wrapper of latest process definition found by name
+	 */
+	public abstract ProcessDefinitionW getProcessDefinition(String processName);
 	
 	public abstract ProcessInstanceW getProcessInstance(long piId);
 	
