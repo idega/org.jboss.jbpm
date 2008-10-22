acc77ef1707e2b29397e08109e1ebf80b2f222f1
Vytautas Civilis <civilis@idega.com>
2008-10-22 15:10:18 +0000

    javadoc added

--- src/java/com/idega/jbpm/exe/ProcessWatch.java
+++ src/java/com/idega/jbpm/exe/ProcessWatch.java
@@ -1,5 +1,13 @@
 package com.idega.jbpm.exe;
 
+/**
+ * assigns/unassigns process to the user's process list (usually used for admins - to put the process into mycases list)
+ * 
+ * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
+ * @version $Revision: 1.2 $
+ *
+ * Last modified: $Date: 2008/10/22 15:10:18 $ by $Author: civilis $
+ */
 public interface ProcessWatch {
 	
 	public abstract boolean takeWatch(Long processInstanceId);
