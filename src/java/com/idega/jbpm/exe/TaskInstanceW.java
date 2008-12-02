d41b6f5a35a23916fd034c74f2404b1b5bd3be3e
Vytautas Civilis <civilis@idega.com>
2008-12-02 13:34:16 +0000

    getView method added

--- src/java/com/idega/jbpm/exe/TaskInstanceW.java
+++ src/java/com/idega/jbpm/exe/TaskInstanceW.java
@@ -13,44 +13,62 @@ import com.idega.user.data.User;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.14 $
- *
- * Last modified: $Date: 2008/10/15 14:56:40 $ by $Author: civilis $
+ * @version $Revision: 1.15 $
+ * 
+ *          Last modified: $Date: 2008/12/02 13:34:16 $ by $Author: civilis $
  */
 public interface TaskInstanceW {
-	
+
 	public abstract void submit(View view);
-	
+
 	public abstract void submit(View view, boolean proceedProcess);
-	
+
 	public abstract void start(int userId);
-	
+
 	public abstract void assign(int userId);
-	
+
 	public abstract void assign(User usr);
-	
+
 	public abstract User getAssignedTo();
-	
-	public abstract View loadView();
-	
-	public abstract Long getTaskInstanceId();
-	
-	public abstract void setTaskInstanceId(Long taskInstanceId);
-	
-	public abstract String getName(Locale locale);
-	
-	public abstract TaskInstance getTaskInstance();
-	
-	public abstract void setTaskRolePermissions(Role role, boolean setSameForAttachments, String variableIdentifier);
-	
+
 	/**
-	 * creates, stores and adds binary variable to the variable specified. If the variable is not found in the process instance 
-	 * (not necessary in the task instance scope), it is created for the task instance.
+	 * loads view for accessing task instance from user interface. Should be
+	 * called for displaying task representation in UI.
+	 * 
+	 * @return
+	 */
+	public abstract View loadView();
+
+	/**
+	 * gets view of task instance The difference between load view, is that load
+	 * view may incorporate some additional actions for viewing task in UI.
+	 * 
+	 * @return
+	 */
+	public abstract View getView();
+
+	public abstract Long getTaskInstanceId();
+
+	public abstract void setTaskInstanceId(Long taskInstanceId);
+
+	public abstract String getName(Locale locale);
+
+	public abstract TaskInstance getTaskInstance();
+
+	public abstract void setTaskRolePermissions(Role role,
+			boolean setSameForAttachments, String variableIdentifier);
+
+	/**
+	 * creates, stores and adds binary variable to the variable specified. If
+	 * the variable is not found in the process instance (not necessary in the
+	 * task instance scope), it is created for the task instance.
+	 * 
 	 * @param variable
 	 * @param fileName
 	 * @param description
 	 * @param is
 	 * @return
 	 */
-	public abstract BinaryVariable addAttachment(Variable variable, String fileName, String description, InputStream is);
+	public abstract BinaryVariable addAttachment(Variable variable,
+			String fileName, String description, InputStream is);
 }
