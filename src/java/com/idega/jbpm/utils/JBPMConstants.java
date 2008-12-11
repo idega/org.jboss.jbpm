1fed43d9922703e7ff3e49293ec32e206de5ac1a
Vytautas Civilis <civilis@idega.com>
2008-12-11 19:29:45 +0000

    bpmLogger added

--- src/java/com/idega/jbpm/utils/JBPMConstants.java
+++ src/java/com/idega/jbpm/utils/JBPMConstants.java
@@ -1,9 +1,14 @@
 package com.idega.jbpm.utils;
 
+import java.util.logging.Logger;
+
 import com.idega.util.CoreConstants;
 
 public class JBPMConstants {
-	
-	public static final String BPM_PATH = CoreConstants.PATH_FILES_ROOT + "/bpm";
+
+	public static final String BPM_PATH = CoreConstants.PATH_FILES_ROOT
+			+ "/bpm";
 	public static final String BPM_COMMENTS_PATH = BPM_PATH + "/comments/";
+
+	public static final Logger bpmLogger = Logger.getLogger("IdegaBPMLogging");
 }
