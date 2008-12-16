2bd853632f1842f19a2c4df05bdc506a4e9cd1cd
Vytautas Civilis <civilis@idega.com>
2008-12-16 19:49:59 +0000

    constants added

--- src/java/com/idega/jbpm/exe/ProcessConstants.java
+++ src/java/com/idega/jbpm/exe/ProcessConstants.java
@@ -2,16 +2,20 @@ package com.idega.jbpm.exe;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.3 $
- *
- * Last modified: $Date: 2008/09/17 13:21:26 $ by $Author: civilis $
+ * @version $Revision: 1.4 $
+ * 
+ *          Last modified: $Date: 2008/12/16 19:49:59 $ by $Author: civilis $
  */
 public final class ProcessConstants {
 
-	private ProcessConstants() {}
-	
+	private ProcessConstants() {
+	}
+
 	public static final String START_PROCESS = "startProcess";
 	public static final String TASK_INSTANCE_ID = "taskInstanceId";
-	
+	public static final String PROCESS_DEFINITION_ID = "processDefinitionId";
+	public static final String VIEW_ID = "viewId";
+	public static final String VIEW_TYPE = "viewType";
+
 	public static final String actionTakenVariableName = "string_actionTaken";
 }
