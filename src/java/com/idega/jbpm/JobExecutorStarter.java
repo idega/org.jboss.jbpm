3c7dccda6649f55ec264033f984381763a4a4c05
Vytautas Civilis <civilis@idega.com>
2008-12-19 08:23:12 +0000

    idle interval placed before starting executor, and using 3 mins

--- src/java/com/idega/jbpm/JobExecutorStarter.java
+++ src/java/com/idega/jbpm/JobExecutorStarter.java
@@ -13,29 +13,32 @@ import com.idega.idegaweb.IWMainApplicationShutdownEvent;
 import com.idega.idegaweb.IWMainApplicationStartedEvent;
 
 /**
- *
+ * 
  * 
  * @author <a href="anton@idega.com">Anton Makarov</a>
- * @version $Revision: 1.3 $, $Date: 2008/12/19 07:45:23 $ by $Author: anton $
- *
+ * @version $Revision: 1.4 $, $Date: 2008/12/19 08:23:12 $ by $Author: civilis $
+ * 
  */
 @Service
 @Scope(BeanDefinition.SCOPE_SINGLETON)
 public class JobExecutorStarter implements ApplicationListener {
 
-	private static final Logger LOGGER = Logger.getLogger(JobExecutorStarter.class.getName());
-	
+	private static final Logger LOGGER = Logger
+			.getLogger(JobExecutorStarter.class.getName());
+
 	public void onApplicationEvent(ApplicationEvent applicationEvent) {
-		if(applicationEvent instanceof IWMainApplicationStartedEvent) {
+
+		if (applicationEvent instanceof IWMainApplicationStartedEvent) {
 			LOGGER.info("Starting Job executor");
+			JbpmConfiguration.getInstance().getJobExecutor().setIdleInterval(
+					180000);
 			JbpmConfiguration.getInstance().startJobExecutor();
-			JbpmConfiguration.getInstance().getJobExecutor().setIdleInterval(18000);
-		} else if(applicationEvent instanceof IWMainApplicationShutdownEvent) {
+		} else if (applicationEvent instanceof IWMainApplicationShutdownEvent) {
 			LOGGER.info("Stopping Job executor");
 			JbpmConfiguration.getInstance().getJobExecutor().stop();
 		}
 	}
-	
+
 	public JbpmConfiguration getJbpmConfiguration() {
 		return JbpmConfiguration.getInstance();
 	}
