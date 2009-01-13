208c23fc353b3e020172904ef35afa223d20455d
Vytautas Civilis <civilis@idega.com>
2009-01-13 13:11:13 +0000

    method bean added

--- src/java/com/idega/jbpm/variables/VariablesResolver.java
+++ src/java/com/idega/jbpm/variables/VariablesResolver.java
@@ -6,6 +6,7 @@ import org.jbpm.graph.exe.ExecutionContext;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import com.idega.util.expression.ELUtil;
 
 @Scope("singleton")
 @Service("bpmVariableResolver")
@@ -13,29 +14,38 @@ public class VariablesResolver {
 
 	private ThreadLocal<ExecutionContext> executionContextLocal = new ThreadLocal<ExecutionContext>();
 	private static Log log = LogFactory.getLog(VariablesResolver.class);
-	
-	
-	public void setExecutionContext(ExecutionContext ectx){
+
+	public void setExecutionContext(ExecutionContext ectx) {
 		executionContextLocal.set(ectx);
 	}
-	
+
 	/**
 	 * Returns a variable with that matching name
 	 * 
 	 * @param variableName
 	 * @return
 	 */
-	public Object get(String variableName){
+	public Object get(String variableName) {
 		ExecutionContext ectx = executionContextLocal.get();
-		if(ectx != null){
-			return ectx.getContextInstance().getVariable(variableName, ectx.getToken());
-		}else{
-			log.warn("Exection context was not set before resolving variable: " + variableName);
-	
-			//TODO: fix this??
+		if (ectx != null) {
+			return ectx.getContextInstance().getVariable(variableName,
+					ectx.getToken());
+		} else {
+			log.warn("Exection context was not set before resolving variable: "
+					+ variableName);
+
+			// TODO: fix this??
 			return null;
 		}
-		
 	}
-	
+
+	/**
+	 * 
+	 * @param beanName
+	 *            just name (not an expression)
+	 * @return bean from Spring or JSF context
+	 */
+	public Object bean(String beanName) {
+		return ELUtil.getInstance().getBean(beanName);
+	}
 }
