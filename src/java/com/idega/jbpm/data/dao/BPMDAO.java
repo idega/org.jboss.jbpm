15f24b57480774eb43ec3dd4fe6816b10ad6bc7d
Vytautas Civilis <civilis@idega.com>
2009-01-18 16:48:46 +0000

    getPermissionsForUser added

--- src/java/com/idega/jbpm/data/dao/BPMDAO.java
+++ src/java/com/idega/jbpm/data/dao/BPMDAO.java
@@ -3,6 +3,7 @@ package com.idega.jbpm.data.dao;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 import org.jbpm.graph.def.ProcessDefinition;
 import org.jbpm.graph.exe.ProcessInstance;
@@ -10,6 +11,7 @@ import org.jbpm.taskmgmt.def.Task;
 
 import com.idega.core.persistence.GenericDao;
 import com.idega.jbpm.data.Actor;
+import com.idega.jbpm.data.ActorPermissions;
 import com.idega.jbpm.data.NativeIdentityBind;
 import com.idega.jbpm.data.ProcessManagerBind;
 import com.idega.jbpm.data.ViewTaskBind;
@@ -17,65 +19,67 @@ import com.idega.jbpm.data.NativeIdentityBind.IdentityType;
 import com.idega.jbpm.identity.Role;
 
 /**
- * serves as DAO, as well as PAO (process access object). In the latter case,
- * this should be used only in cases, when explicit access to process is
- * required, but working with -wrapper- objects (e.g. ProcessDefinitionW),
- * ProcessManager and so on is recommended
+ * serves as DAO, as well as PAO (process access object). In the latter case, this should be used
+ * only in cases, when explicit access to process is required, but working with -wrapper- objects
+ * (e.g. ProcessDefinitionW), ProcessManager and so on is recommended
  * 
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.17 $
- * 
- *          Last modified: $Date: 2009/01/13 10:48:07 $ by $Author: anton $
+ * @version $Revision: 1.18 $ Last modified: $Date: 2009/01/18 16:48:46 $ by $Author: civilis $
  */
 public interface BPMDAO extends GenericDao {
-
-	public abstract ViewTaskBind getViewTaskBind(long taskId, String viewType);
-
-	public abstract ViewTaskBind getViewTaskBindByTaskInstance(
-			long taskInstanceId, String viewType);
-
-	public abstract List<ViewTaskBind> getViewTaskBindsByTaskId(long taskId);
-
-	public abstract List<ViewTaskBind> getViewTaskBindsByTaskInstanceId(
-			long taskInstanceId);
-
-	public abstract ViewTaskBind getViewTaskBindByView(String viewId,
-			String viewType);
-
-	public abstract List<ViewTaskBind> getViewTaskBindsByTasksIds(
-			Collection<Long> taskIds);
-
-	public abstract Task getTaskFromViewTaskBind(ViewTaskBind viewTaskBind);
-
-	public abstract ProcessManagerBind getProcessManagerBind(String processName);
-
-	public abstract List<Actor> getAllGeneralProcessRoles();
-
-	public abstract void updateAddGrpsToRole(Long roleActorId,
-			Collection<String> selectedGroupsIds);
-
-	public abstract List<NativeIdentityBind> getNativeIdentities(
-			long processRoleIdentityId);
-
-	public abstract List<Actor> getProcessRoles(Collection<Long> actorIds);
-
-	public abstract List<NativeIdentityBind> getNativeIdentities(
-			Collection<Long> actorsIds, IdentityType identityType);
-
-	public abstract void updateCreateProcessRoles(Collection<Role> rolesNames,
-			Long processInstanceId);
-
-	public abstract List<Object[]> getProcessTasksViewsInfos(
-			Collection<Long> processDefinitionsIds, String viewType);
-
-	public abstract List<Actor> getProcessRoles(Collection<String> rolesNames,
-			Long processInstanceId);
-
-	public abstract List<ProcessInstance> getSubprocessInstancesOneLevel(
-			long parentProcessInstanceId);
-
-	public abstract ProcessDefinition findLatestProcessDefinition(
-			final String processName);
 	
-	public abstract List<Long> getTaskInstancesByActorIdAndDate(Long actorId, Date timestamp);
+	public abstract ViewTaskBind getViewTaskBind(long taskId, String viewType);
+	
+	public abstract ViewTaskBind getViewTaskBindByTaskInstance(
+	        long taskInstanceId, String viewType);
+	
+	public abstract List<ViewTaskBind> getViewTaskBindsByTaskId(long taskId);
+	
+	public abstract List<ViewTaskBind> getViewTaskBindsByTaskInstanceId(
+	        long taskInstanceId);
+	
+	public abstract ViewTaskBind getViewTaskBindByView(String viewId,
+	        String viewType);
+	
+	public abstract List<ViewTaskBind> getViewTaskBindsByTasksIds(
+	        Collection<Long> taskIds);
+	
+	public abstract Task getTaskFromViewTaskBind(ViewTaskBind viewTaskBind);
+	
+	public abstract ProcessManagerBind getProcessManagerBind(String processName);
+	
+	public abstract List<Actor> getAllGeneralProcessRoles();
+	
+	public abstract void updateAddGrpsToRole(Long roleActorId,
+	        Collection<String> selectedGroupsIds);
+	
+	public abstract List<NativeIdentityBind> getNativeIdentities(
+	        long processRoleIdentityId);
+	
+	public abstract List<Actor> getProcessRoles(Collection<Long> actorIds);
+	
+	public abstract List<NativeIdentityBind> getNativeIdentities(
+	        Collection<Long> actorsIds, IdentityType identityType);
+	
+	public abstract void updateCreateProcessRoles(Collection<Role> rolesNames,
+	        Long processInstanceId);
+	
+	public abstract List<Object[]> getProcessTasksViewsInfos(
+	        Collection<Long> processDefinitionsIds, String viewType);
+	
+	public abstract List<Actor> getProcessRoles(Collection<String> rolesNames,
+	        Long processInstanceId);
+	
+	public abstract List<ProcessInstance> getSubprocessInstancesOneLevel(
+	        long parentProcessInstanceId);
+	
+	public abstract ProcessDefinition findLatestProcessDefinition(
+	        final String processName);
+	
+	public abstract List<Long> getTaskInstancesByActorIdAndDate(Long actorId,
+	        Date timestamp);
+	
+	public abstract List<ActorPermissions> getPermissionsForUser(
+	        Integer userId, String processName, Long processInstanceId,
+	        Set<String> userNativeRoles, Set<String> userGroupsIds);
 }
--- src/java/com/idega/jbpm/data/dao/impl/BPMDAOImpl.java
+++ src/java/com/idega/jbpm/data/dao/impl/BPMDAOImpl.java
@@ -3,7 +3,9 @@ package com.idega.jbpm.data.dao.impl;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.jbpm.JbpmContext;
 import org.jbpm.JbpmException;
@@ -20,279 +22,348 @@ import com.idega.core.persistence.impl.GenericDaoImpl;
 import com.idega.jbpm.BPMContext;
 import com.idega.jbpm.JbpmCallback;
 import com.idega.jbpm.data.Actor;
+import com.idega.jbpm.data.ActorPermissions;
 import com.idega.jbpm.data.NativeIdentityBind;
 import com.idega.jbpm.data.ProcessManagerBind;
 import com.idega.jbpm.data.ViewTaskBind;
 import com.idega.jbpm.data.NativeIdentityBind.IdentityType;
 import com.idega.jbpm.data.dao.BPMDAO;
 import com.idega.jbpm.identity.Role;
+import com.idega.util.ListUtil;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.25 $
- * 
- *          Last modified: $Date: 2009/01/13 10:48:25 $ by $Author: anton $
+ * @version $Revision: 1.26 $ Last modified: $Date: 2009/01/18 16:48:46 $ by $Author: civilis $
  */
 @Scope("singleton")
 @Repository("bpmBindsDAO")
 @Transactional(readOnly = true)
 public class BPMDAOImpl extends GenericDaoImpl implements BPMDAO {
-
+	
 	@Autowired
 	private BPMContext bpmContext;
-
+	
 	public ViewTaskBind getViewTaskBind(long taskId, String viewType) {
-
+		
 		@SuppressWarnings("unchecked")
 		List<ViewTaskBind> binds = getEntityManager().createNamedQuery(
-				ViewTaskBind.GET_UNIQUE_BY_TASK_ID_AND_VIEW_TYPE_QUERY_NAME)
-				.setParameter(ViewTaskBind.taskIdParam, taskId).setParameter(
-						ViewTaskBind.viewTypeParam, viewType).getResultList();
-
+		    ViewTaskBind.GET_UNIQUE_BY_TASK_ID_AND_VIEW_TYPE_QUERY_NAME)
+		        .setParameter(ViewTaskBind.taskIdParam, taskId).setParameter(
+		            ViewTaskBind.viewTypeParam, viewType).getResultList();
+		
 		return binds.isEmpty() ? null : binds.iterator().next();
 	}
-
+	
 	public ViewTaskBind getViewTaskBindByTaskInstance(long taskInstanceId,
-			String viewType) {
-
+	        String viewType) {
+		
 		@SuppressWarnings("unchecked")
 		List<ViewTaskBind> binds = getEntityManager()
-				.createNamedQuery(
-						ViewTaskBind.GET_UNIQUE_BY_TASK_INSTANCE_ID_AND_VIEW_TYPE_QUERY_NAME)
-				.setParameter(ViewTaskBind.taskInstanceIdProp, taskInstanceId)
-				.setParameter(ViewTaskBind.viewTypeParam, viewType)
-				.getResultList();
-
+		        .createNamedQuery(
+		            ViewTaskBind.GET_UNIQUE_BY_TASK_INSTANCE_ID_AND_VIEW_TYPE_QUERY_NAME)
+		        .setParameter(ViewTaskBind.taskInstanceIdProp, taskInstanceId)
+		        .setParameter(ViewTaskBind.viewTypeParam, viewType)
+		        .getResultList();
+		
 		return binds.isEmpty() ? null : binds.iterator().next();
 	}
-
+	
 	public List<ViewTaskBind> getViewTaskBindsByTaskId(long taskId) {
-
+		
 		@SuppressWarnings("unchecked")
 		List<ViewTaskBind> binds = getEntityManager().createNamedQuery(
-				ViewTaskBind.getViewTaskBindsByTaskId).setParameter(
-				ViewTaskBind.taskIdParam, taskId).getResultList();
-
+		    ViewTaskBind.getViewTaskBindsByTaskId).setParameter(
+		    ViewTaskBind.taskIdParam, taskId).getResultList();
+		
 		return binds;
 	}
-
+	
 	public List<ViewTaskBind> getViewTaskBindsByTaskInstanceId(
-			long taskInstanceId) {
-
+	        long taskInstanceId) {
+		
 		@SuppressWarnings("unchecked")
 		List<ViewTaskBind> binds = getEntityManager().createNamedQuery(
-				ViewTaskBind.getViewTaskBindsByTaskInstanceId).setParameter(
-				ViewTaskBind.taskInstanceIdProp, taskInstanceId)
-				.getResultList();
-
+		    ViewTaskBind.getViewTaskBindsByTaskInstanceId).setParameter(
+		    ViewTaskBind.taskInstanceIdProp, taskInstanceId).getResultList();
+		
 		return binds;
 	}
-
+	
 	public ViewTaskBind getViewTaskBindByView(String viewId, String viewType) {
-
+		
 		return getSingleResult(
-				ViewTaskBind.GET_VIEW_TASK_BIND_BY_VIEW_QUERY_NAME,
-				ViewTaskBind.class,
-				new Param(ViewTaskBind.viewIdParam, viewId), new Param(
-						ViewTaskBind.viewTypeParam, viewType));
+		    ViewTaskBind.GET_VIEW_TASK_BIND_BY_VIEW_QUERY_NAME,
+		    ViewTaskBind.class, new Param(ViewTaskBind.viewIdParam, viewId),
+		    new Param(ViewTaskBind.viewTypeParam, viewType));
 	}
-
+	
 	public List<ViewTaskBind> getViewTaskBindsByTasksIds(
-			Collection<Long> taskIds) {
-
+	        Collection<Long> taskIds) {
+		
 		@SuppressWarnings("unchecked")
 		List<ViewTaskBind> viewTaskBinds = getEntityManager().createNamedQuery(
-				ViewTaskBind.GET_VIEW_TASK_BINDS_BY_TASKS_IDS).setParameter(
-				ViewTaskBind.tasksIdsParam, taskIds).getResultList();
-
+		    ViewTaskBind.GET_VIEW_TASK_BINDS_BY_TASKS_IDS).setParameter(
+		    ViewTaskBind.tasksIdsParam, taskIds).getResultList();
+		
 		return viewTaskBinds;
 	}
-
+	
 	public Task getTaskFromViewTaskBind(ViewTaskBind viewTaskBind) {
-
+		
 		return (Task) getEntityManager().createNamedQuery(
-				ViewTaskBind.GET_VIEW_TASK).setParameter(
-				ViewTaskBind.viewTypeParam, viewTaskBind.getViewType())
-				.setParameter(ViewTaskBind.taskIdParam,
-						viewTaskBind.getTaskId()).getSingleResult();
+		    ViewTaskBind.GET_VIEW_TASK).setParameter(
+		    ViewTaskBind.viewTypeParam, viewTaskBind.getViewType())
+		        .setParameter(ViewTaskBind.taskIdParam,
+		            viewTaskBind.getTaskId()).getSingleResult();
 	}
-
+	
 	public ProcessManagerBind getProcessManagerBind(String processName) {
-
+		
 		ProcessManagerBind pmb = getSingleResult(
-				ProcessManagerBind.getByProcessName, ProcessManagerBind.class,
-				new Param(ProcessManagerBind.processNameProp, processName));
-
+		    ProcessManagerBind.getByProcessName, ProcessManagerBind.class,
+		    new Param(ProcessManagerBind.processNameProp, processName));
+		
 		return pmb;
 	}
-
+	
 	public List<Actor> getAllGeneralProcessRoles() {
-
+		
 		@SuppressWarnings("unchecked")
 		List<Actor> all = getEntityManager().createNamedQuery(
-				Actor.getAllGeneral).getResultList();
-
+		    Actor.getAllGeneral).getResultList();
+		
 		return all;
 	}
-
+	
 	public List<Actor> getProcessRoles(Collection<Long> actorIds) {
-
+		
 		if (actorIds == null || actorIds.isEmpty())
 			throw new IllegalArgumentException("ActorIds should contain values");
-
+		
 		@SuppressWarnings("unchecked")
 		List<Actor> all = getEntityManager().createNamedQuery(
-				Actor.getAllByActorIds).setParameter(Actor.actorIdProperty,
-				actorIds).getResultList();
-
+		    Actor.getAllByActorIds).setParameter(Actor.actorIdProperty,
+		    actorIds).getResultList();
+		
 		return all;
 	}
-
+	
 	@Transactional(readOnly = false)
 	public void updateAddGrpsToRole(Long roleActorId,
-			Collection<String> selectedGroupsIds) {
-
+	        Collection<String> selectedGroupsIds) {
+		
 		Actor roleIdentity = find(Actor.class, roleActorId);
-
+		
 		List<NativeIdentityBind> nativeIdentities = new ArrayList<NativeIdentityBind>(
-				selectedGroupsIds.size());
-
+		        selectedGroupsIds.size());
+		
 		for (String groupId : selectedGroupsIds) {
-
+			
 			NativeIdentityBind nativeIdentity = new NativeIdentityBind();
 			nativeIdentity.setIdentityId(groupId);
 			nativeIdentity.setIdentityType(IdentityType.GROUP);
 			nativeIdentity.setActor(roleIdentity);
 			nativeIdentities.add(nativeIdentity);
 		}
-
+		
 		List<NativeIdentityBind> existingNativeIdentities = roleIdentity
-				.getNativeIdentities();
+		        .getNativeIdentities();
 		List<Long> nativeIdentitiesToRemove = new ArrayList<Long>();
-
+		
 		if (existingNativeIdentities != null) {
-
+			
 			for (NativeIdentityBind existing : existingNativeIdentities) {
-
+				
 				if (nativeIdentities.contains(existing)) {
-
+					
 					nativeIdentities.remove(existing);
 					nativeIdentities.add(existing);
 				} else {
-
+					
 					nativeIdentitiesToRemove.add(existing.getId());
 				}
 			}
 		} else {
 			existingNativeIdentities = new ArrayList<NativeIdentityBind>();
 		}
-
+		
 		roleIdentity.setNativeIdentities(nativeIdentities);
 		getEntityManager().merge(roleIdentity);
-
+		
 		if (!nativeIdentitiesToRemove.isEmpty())
 			getEntityManager().createNamedQuery(NativeIdentityBind.deleteByIds)
-					.setParameter(NativeIdentityBind.idsParam,
-							nativeIdentitiesToRemove).executeUpdate();
+			        .setParameter(NativeIdentityBind.idsParam,
+			            nativeIdentitiesToRemove).executeUpdate();
 	}
-
+	
 	public List<NativeIdentityBind> getNativeIdentities(
-			long processRoleIdentityId) {
-
+	        long processRoleIdentityId) {
+		
 		@SuppressWarnings("unchecked")
 		List<NativeIdentityBind> binds = getEntityManager().createNamedQuery(
-				NativeIdentityBind.getByProcIdentity).setParameter(
-				NativeIdentityBind.procIdentityParam, processRoleIdentityId)
-				.getResultList();
-
+		    NativeIdentityBind.getByProcIdentity).setParameter(
+		    NativeIdentityBind.procIdentityParam, processRoleIdentityId)
+		        .getResultList();
+		
 		return binds;
 	}
-
+	
 	public List<NativeIdentityBind> getNativeIdentities(
-			Collection<Long> actorsIds, IdentityType identityType) {
-
+	        Collection<Long> actorsIds, IdentityType identityType) {
+		
 		@SuppressWarnings("unchecked")
 		List<NativeIdentityBind> binds = getEntityManager().createNamedQuery(
-				NativeIdentityBind.getByTypesAndProceIdentities).setParameter(
-				NativeIdentityBind.identityTypeProperty, identityType)
-				.setParameter(Actor.actorIdProperty, actorsIds).getResultList();
-
+		    NativeIdentityBind.getByTypesAndProceIdentities).setParameter(
+		    NativeIdentityBind.identityTypeProperty, identityType)
+		        .setParameter(Actor.actorIdProperty, actorsIds).getResultList();
+		
 		return binds;
 	}
-
+	
 	@Transactional(readOnly = false)
 	public void updateCreateProcessRoles(Collection<Role> rolesNames,
-			Long processInstanceId) {
-
+	        Long processInstanceId) {
+		
 		for (Role role : rolesNames) {
-
+			
 			Actor prole = new Actor();
 			prole.setProcessRoleName(role.getRoleName());
 			prole.setProcessInstanceId(processInstanceId);
-
+			
 			persist(prole);
 		}
 	}
-
+	
 	public List<Object[]> getProcessTasksViewsInfos(
-			Collection<Long> processDefinitionsIds, String viewType) {
-
+	        Collection<Long> processDefinitionsIds, String viewType) {
+		
 		if (processDefinitionsIds == null || processDefinitionsIds.isEmpty()
-				|| viewType == null)
+		        || viewType == null)
 			return new ArrayList<Object[]>(0);
-
+		
 		@SuppressWarnings("unchecked")
 		List<Object[]> viewsInfos = getEntityManager().createNamedQuery(
-				ViewTaskBind.GET_PROCESS_TASK_VIEW_INFO).setParameter(
-				ViewTaskBind.processDefIdsParam, processDefinitionsIds)
-				.setParameter(ViewTaskBind.viewTypeProp, viewType)
-				.getResultList();
-
+		    ViewTaskBind.GET_PROCESS_TASK_VIEW_INFO).setParameter(
+		    ViewTaskBind.processDefIdsParam, processDefinitionsIds)
+		        .setParameter(ViewTaskBind.viewTypeProp, viewType)
+		        .getResultList();
+		
 		return viewsInfos;
 	}
-
+	
 	public List<Actor> getProcessRoles(Collection<String> rolesNames,
-			Long processInstanceId) {
-
+	        Long processInstanceId) {
+		
 		List<Actor> proles = getResultList(Actor.getSetByRoleNamesAndPIId,
-				Actor.class, new Param(Actor.processRoleNameProperty,
-						rolesNames), new Param(Actor.processInstanceIdProperty,
-						processInstanceId));
-
+		    Actor.class, new Param(Actor.processRoleNameProperty, rolesNames),
+		    new Param(Actor.processInstanceIdProperty, processInstanceId));
+		
 		return proles;
 	}
-
+	
 	public List<ProcessInstance> getSubprocessInstancesOneLevel(
-			long parentProcessInstanceId) {
-
+	        long parentProcessInstanceId) {
+		
 		List<ProcessInstance> subprocesses = getResultList(
-				ProcessManagerBind.getSubprocessesOneLevel,
-				ProcessInstance.class, new Param(
-						ProcessManagerBind.processInstanceIdParam,
-						parentProcessInstanceId));
-
+		    ProcessManagerBind.getSubprocessesOneLevel, ProcessInstance.class,
+		    new Param(ProcessManagerBind.processInstanceIdParam,
+		            parentProcessInstanceId));
+		
 		return subprocesses;
 	}
-
+	
 	public ProcessDefinition findLatestProcessDefinition(
-			final String processName) {
-
+	        final String processName) {
+		
 		return getBpmContext().execute(new JbpmCallback() {
-
+			
 			public Object doInJbpm(JbpmContext context) throws JbpmException {
-
+				
 				return context.getGraphSession().findLatestProcessDefinition(
-						processName);
+				    processName);
 			}
 		});
 	}
-
+	
 	BPMContext getBpmContext() {
 		return bpmContext;
 	}
-
-	public List<Long> getTaskInstancesByActorIdAndDate(Long actorId, Date timestamp) {
-		List<Long> taskInstances = getResultList(Actor.getTaskInstancesByUserRole, Long.class, new Param(Actor.actorIdProperty, actorId.toString()), new Param(Actor.createParam, timestamp));
+	
+	public List<Long> getTaskInstancesByActorIdAndDate(Long actorId,
+	        Date timestamp) {
+		List<Long> taskInstances = getResultList(
+		    Actor.getTaskInstancesByUserRole, Long.class, new Param(
+		            Actor.actorIdProperty, actorId.toString()), new Param(
+		            Actor.createParam, timestamp));
 		return taskInstances;
+	}
+	
+	public List<ActorPermissions> getPermissionsForUser(Integer userId,
+	        String processName, Long processInstanceId,
+	        Set<String> userNativeRoles, Set<String> userGroupsIds) {
+		
+		if (userGroupsIds != null) {
+			throw new UnsupportedOperationException(
+			        "Searching by user groups not supported yet");
+		}
+		
+		if(processName != null) {
+			throw new UnsupportedOperationException("Searching by process name not supported yet");
+		}
+		
+		if(ListUtil.isEmpty(userNativeRoles)) {
+
+//			this is perhaps silly, but just because we don't want to maintain two queries just for the case, when user doesn't have any roles
+//			this is the case only for bpmUser usually
+			userNativeRoles = new HashSet<String>(1);
+			userNativeRoles.add("mock2345324659324");
+		}
+		
+		String identityTypeRoleParam = "identityTypeRole";
+		String identityIdsRolesParam = "identityIdsRoles";
+		
+		List<ActorPermissions> perms = getResultListByInlineQuery(
+			
+		    "select perms from com.idega.jbpm.data.Actor a inner join a."+ Actor.nativeIdentitiesProperty+ " ni inner join a."+Actor.actorPermissionsProperty+" perms "+
+		    "where a."+Actor.processInstanceIdProperty+" = :"+Actor.processInstanceIdProperty+" and " +
+		    		"((ni."+NativeIdentityBind.identityTypeProperty+" = :"+NativeIdentityBind.identityTypeProperty+" and ni."+NativeIdentityBind.identityIdProperty+" = :"+NativeIdentityBind.identityIdProperty+") " +
+		    		"or (ni."+NativeIdentityBind.identityTypeProperty+" = :"+identityTypeRoleParam+" and ni."+NativeIdentityBind.identityIdProperty+" in (:"+identityIdsRolesParam+")))"
+		    		
+		            , ActorPermissions.class, 
+		            new Param(Actor.processInstanceIdProperty, processInstanceId),
+		            new Param(NativeIdentityBind.identityTypeProperty, NativeIdentityBind.IdentityType.USER),
+		            new Param(NativeIdentityBind.identityIdProperty, userId.toString()),
+		            new Param(identityTypeRoleParam, NativeIdentityBind.IdentityType.ROLE),
+		            new Param(identityIdsRolesParam, userNativeRoles));
+		
+//		TODO: merge with roles (backward)
+		List<Actor> globalRolesActors = getResultListByInlineQuery(
+			
+//			a."+ Actor.nativeIdentitiesProperty+ "
+			
+		    "select a from com.idega.jbpm.data.Actor a "+
+		    "where a."+Actor.processInstanceIdProperty+" = :"+Actor.processInstanceIdProperty+
+		    " and a."+Actor.processRoleNameProperty+" in(:"+Actor.processRoleNameProperty+")"
+		            , Actor.class, 
+		            new Param(Actor.processInstanceIdProperty, processInstanceId),
+		            new Param(Actor.processRoleNameProperty, userNativeRoles)
+		);
+		
+		if(globalRolesActors != null) {
+			
+			for (Actor actor : globalRolesActors) {
+		        
+				if(ListUtil.isEmpty(actor.getNativeIdentities())) {
+					
+//					this is in old way the pd scope actor
+
+					if(actor.getActorPermissions() != null) {
+						perms.addAll(actor.getActorPermissions());
+					}
+				}
+	        }	
+		}
+		
+		return perms;
 	}
 }
