dbd24aaac8013186851f43122ab3618930108922
Vytautas Civilis <civilis@idega.com>
2009-01-18 16:48:11 +0000

    addActor added

--- src/java/com/idega/jbpm/data/ActorPermissions.java
+++ src/java/com/idega/jbpm/data/ActorPermissions.java
@@ -1,7 +1,7 @@
 package com.idega.jbpm.data;
 
-
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -16,29 +16,87 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 /**
- * Actor permissions for task or taskInstance. TaskInstance permissions should override ones specified for Task.
- * If variableName is not null, then for taskInstance or task, the most permissive permission should be used.
- * Also used for contacts access management.
+ * Actor permissions for task or taskInstance. TaskInstance permissions should override ones
+ * specified for Task. If variableName is not null, then for taskInstance or task, the most
+ * permissive permission should be used. Also used for contacts access management.
  * 
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.11 $
- *
- * Last modified: $Date: 2008/10/22 15:09:38 $ by $Author: civilis $
+ * @version $Revision: 1.12 $ Last modified: $Date: 2009/01/18 16:48:11 $ by $Author: civilis $
  */
 @Entity
-@Table(name="BPM_ACTORS_PERMISSIONS")
-@NamedQueries(
-		{
-			@NamedQuery(name=ActorPermissions.getSetByTaskIdAndProcessRoleNames, query="from ActorPermissions ap where ap."+ActorPermissions.taskIdProperty+" = :"+ActorPermissions.taskIdProperty+" and "+ActorPermissions.roleNameProperty+" in (:"+ActorPermissions.roleNameProperty+")"),
-			@NamedQuery(name=ActorPermissions.getSetByTaskIdOrTaskInstanceId, query="from ActorPermissions ap where ap."+ActorPermissions.taskIdProperty+" = :"+ActorPermissions.taskIdProperty+" or ap."+ActorPermissions.taskInstanceIdProperty+" = :"+ActorPermissions.taskInstanceIdProperty),
-			@NamedQuery(name=ActorPermissions.getSetByProcessRoleNamesAndProcessInstanceIdPureRoles, query="from ActorPermissions ap inner join ap." + ActorPermissions.actorsProperty + " actors where ap."+ActorPermissions.roleNameProperty+" in (:"+ActorPermissions.roleNameProperty+") and actors." + Actor.processInstanceIdProperty + " = :" + Actor.processInstanceIdProperty +" and ap."+ActorPermissions.taskIdProperty+" is null and ap."+ActorPermissions.taskInstanceIdProperty+" is null and ap."+ActorPermissions.canSeeContactsOfRoleNameProperty+" is null"),
-			@NamedQuery(name=ActorPermissions.getSetByProcessRoleNamesAndProcessInstanceIdForContacts, query="from ActorPermissions ap inner join ap." + ActorPermissions.actorsProperty + " actors where ap."+ActorPermissions.roleNameProperty+" in (:"+ActorPermissions.roleNameProperty+") and actors." + Actor.processInstanceIdProperty + " = :" + Actor.processInstanceIdProperty +" and ap."+ActorPermissions.taskIdProperty+" is null and ap."+ActorPermissions.taskInstanceIdProperty+" is null and ap."+ActorPermissions.canSeeContactsOfRoleNameProperty+" is not null"),
-			@NamedQuery(name=ActorPermissions.getSetByProcessInstanceIdAndContactPermissionsRolesNames, query="select ap from ActorPermissions ap inner join ap."+ActorPermissions.actorsProperty+" actors where actors." + Actor.processInstanceIdProperty + " = :" + Actor.processInstanceIdProperty +" and ap."+ActorPermissions.canSeeContactsOfRoleNameProperty+" in (:"+ActorPermissions.canSeeContactsOfRoleNameProperty+")"),
-			@NamedQuery(name=ActorPermissions.getSetByProcessInstanceIdAndCanSeeContacts, query="select ap from ActorPermissions ap inner join ap."+ActorPermissions.actorsProperty+" act where act." + Actor.processInstanceIdProperty + " = :" + Actor.processInstanceIdProperty +" and ap."+ActorPermissions.canSeeContactsOfRoleNameProperty+" is not null")
-		}
-)
+@Table(name = "BPM_ACTORS_PERMISSIONS")
+@NamedQueries( {
+        @NamedQuery(name = ActorPermissions.getSetByTaskIdAndProcessRoleNames, query = "from ActorPermissions ap where ap."
+                + ActorPermissions.taskIdProperty
+                + " = :"
+                + ActorPermissions.taskIdProperty
+                + " and "
+                + ActorPermissions.roleNameProperty
+                + " in (:"
+                + ActorPermissions.roleNameProperty + ")"),
+        @NamedQuery(name = ActorPermissions.getSetByTaskIdOrTaskInstanceId, query = "from ActorPermissions ap where ap."
+                + ActorPermissions.taskIdProperty
+                + " = :"
+                + ActorPermissions.taskIdProperty
+                + " or ap."
+                + ActorPermissions.taskInstanceIdProperty
+                + " = :"
+                + ActorPermissions.taskInstanceIdProperty),
+        @NamedQuery(name = ActorPermissions.getSetByProcessRoleNamesAndProcessInstanceIdPureRoles, query = "from ActorPermissions ap inner join ap."
+                + ActorPermissions.actorsProperty
+                + " actors where ap."
+                + ActorPermissions.roleNameProperty
+                + " in (:"
+                + ActorPermissions.roleNameProperty
+                + ") and actors."
+                + Actor.processInstanceIdProperty
+                + " = :"
+                + Actor.processInstanceIdProperty
+                + " and ap."
+                + ActorPermissions.taskIdProperty
+                + " is null and ap."
+                + ActorPermissions.taskInstanceIdProperty
+                + " is null and ap."
+                + ActorPermissions.canSeeContactsOfRoleNameProperty
+                + " is null"),
+        @NamedQuery(name = ActorPermissions.getSetByProcessRoleNamesAndProcessInstanceIdForContacts, query = "from ActorPermissions ap inner join ap."
+                + ActorPermissions.actorsProperty
+                + " actors where ap."
+                + ActorPermissions.roleNameProperty
+                + " in (:"
+                + ActorPermissions.roleNameProperty
+                + ") and actors."
+                + Actor.processInstanceIdProperty
+                + " = :"
+                + Actor.processInstanceIdProperty
+                + " and ap."
+                + ActorPermissions.taskIdProperty
+                + " is null and ap."
+                + ActorPermissions.taskInstanceIdProperty
+                + " is null and ap."
+                + ActorPermissions.canSeeContactsOfRoleNameProperty
+                + " is not null"),
+        @NamedQuery(name = ActorPermissions.getSetByProcessInstanceIdAndContactPermissionsRolesNames, query = "select ap from ActorPermissions ap inner join ap."
+                + ActorPermissions.actorsProperty
+                + " actors where actors."
+                + Actor.processInstanceIdProperty
+                + " = :"
+                + Actor.processInstanceIdProperty
+                + " and ap."
+                + ActorPermissions.canSeeContactsOfRoleNameProperty
+                + " in (:"
+                + ActorPermissions.canSeeContactsOfRoleNameProperty + ")"),
+        @NamedQuery(name = ActorPermissions.getSetByProcessInstanceIdAndCanSeeContacts, query = "select ap from ActorPermissions ap inner join ap."
+                + ActorPermissions.actorsProperty
+                + " act where act."
+                + Actor.processInstanceIdProperty
+                + " = :"
+                + Actor.processInstanceIdProperty
+                + " and ap."
+                + ActorPermissions.canSeeContactsOfRoleNameProperty
+                + " is not null") })
 public class ActorPermissions implements Serializable {
-
+	
 	private static final long serialVersionUID = 4768266953928292205L;
 	
 	public static final String getSetByTaskIdAndProcessRoleNames = "ActorPermissions.getSetByTaskIdAndProcessRoleNames";
@@ -48,127 +106,158 @@ public class ActorPermissions implements Serializable {
 	public static final String getSetByProcessInstanceIdAndContactPermissionsRolesNames = "ActorPermissions.getSetByProcessInstanceIdAndContactPermissionsRolesNames";
 	public static final String getSetByProcessInstanceIdAndAccess = "ActorPermissions.getSetByProcessInstanceIdAndAccess";
 	public static final String getSetByProcessInstanceIdAndCanSeeContacts = "ActorPermissions.getSetByProcessInstanceIdAndCanSeeContacts";
-
-	@Id @GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name="actperm_id")
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "actperm_id")
 	private Long id;
 	
 	public static final String taskIdProperty = "taskId";
-	@Column(name="task_id")
+	@Column(name = "task_id")
 	private Long taskId;
 	
 	public static final String taskInstanceIdProperty = "taskInstanceId";
-	@Column(name="task_instance_id")
+	@Column(name = "task_instance_id")
 	private Long taskInstanceId;
 	
-	@Column(name="variable_identifier")
+	@Column(name = "variable_identifier")
 	private String variableIdentifier;
 	
 	public static final String roleNameProperty = "roleName";
-	@Column(name="process_role_name")
+	@Column(name = "process_role_name")
 	private String roleName;
 	
 	public static final String readPermissionProperty = "readPermission";
-	@Column(name="has_read_permission")
+	@Column(name = "has_read_permission")
 	private Boolean readPermission;
 	
 	public static final String writePermissionProperty = "writePermission";
-	@Column(name="has_write_permission")
+	@Column(name = "has_write_permission")
 	private Boolean writePermission;
 	
 	public static final String modifyRightsPermissionProperty = "modifyRightsPermission";
-	@Column(name="can_modify_rights_permission")
+	@Column(name = "can_modify_rights_permission")
 	private Boolean modifyRightsPermission;
 	
 	public static final String caseHandlerPermissionProperty = "caseHandlerPermission";
-	@Column(name="case_handler_permission")
+	@Column(name = "case_handler_permission")
 	private Boolean caseHandlerPermission;
 	
 	public static final String canSeeContactsOfRoleNameProperty = "canSeeContactsOfRoleName";
-	@Column(name="can_see_contacts_of_role_name")
+	@Column(name = "can_see_contacts_of_role_name")
 	private String canSeeContactsOfRoleName;
 	
 	public static final String canSeeContactsProperty = "canSeeContacts";
-	@Column(name="can_see_contacts")
+	@Column(name = "can_see_contacts")
 	private Boolean canSeeContacts;
 	
 	public static final String actorsProperty = "actors";
-	@ManyToMany(
-	        cascade = {CascadeType.PERSIST, CascadeType.MERGE},
-	        mappedBy = Actor.actorPermissionsProperty,
-	        targetEntity = Actor.class
-	)
+	@ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE }, mappedBy = Actor.actorPermissionsProperty, targetEntity = Actor.class)
 	private List<Actor> actors;
 	
 	public List<Actor> getActors() {
 		return actors;
 	}
+	
 	public void setActors(List<Actor> actors) {
 		this.actors = actors;
 	}
+	
+	public void addActor(Actor actor) {
+		List<Actor> actors = getActors();
+		
+		if (actors == null) {
+			actors = new ArrayList<Actor>();
+			setActors(actors);
+		}
+		
+		actors.add(actor);
+	}
+	
 	public Long getId() {
 		return id;
 	}
+	
 	protected void setId(Long id) {
 		this.id = id;
 	}
+	
 	public Long getTaskId() {
 		return taskId;
 	}
+	
 	public void setTaskId(Long taskId) {
 		this.taskId = taskId;
 	}
+	
 	public Long getTaskInstanceId() {
 		return taskInstanceId;
 	}
+	
 	public void setTaskInstanceId(Long taskInstanceId) {
 		this.taskInstanceId = taskInstanceId;
 	}
+	
 	public Boolean getReadPermission() {
 		return readPermission;
 	}
+	
 	public void setReadPermission(Boolean readPermission) {
 		this.readPermission = readPermission;
 	}
+	
 	public Boolean getWritePermission() {
 		return writePermission;
 	}
+	
 	public void setWritePermission(Boolean writePermission) {
 		this.writePermission = writePermission;
 	}
+	
 	public Boolean getModifyRightsPermission() {
 		return modifyRightsPermission;
 	}
+	
 	public void setModifyRightsPermission(Boolean modifyRightsPermission) {
 		this.modifyRightsPermission = modifyRightsPermission;
 	}
+	
 	public String getRoleName() {
 		return roleName;
 	}
+	
 	public void setRoleName(String roleName) {
 		this.roleName = roleName;
 	}
+	
 	public String getVariableIdentifier() {
 		return variableIdentifier;
 	}
+	
 	public void setVariableIdentifier(String variableIdentifier) {
 		this.variableIdentifier = variableIdentifier;
 	}
+	
 	public String getCanSeeContactsOfRoleName() {
 		return canSeeContactsOfRoleName;
 	}
+	
 	public void setCanSeeContactsOfRoleName(String canSeeContactsOfRoleName) {
 		this.canSeeContactsOfRoleName = canSeeContactsOfRoleName;
 	}
+	
 	public Boolean getCaseHandlerPermission() {
 		return caseHandlerPermission;
 	}
+	
 	public void setCaseHandlerPermission(Boolean caseHandlerPermission) {
 		this.caseHandlerPermission = caseHandlerPermission;
 	}
+	
 	public Boolean getCanSeeContacts() {
 		return canSeeContacts;
 	}
+	
 	public void setCanSeeContacts(Boolean canSeeContacts) {
 		this.canSeeContacts = canSeeContacts;
 	}
