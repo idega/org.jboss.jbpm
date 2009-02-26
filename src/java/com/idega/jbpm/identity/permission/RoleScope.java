357a3ed2462ca2bb934969f10cc61148b2762fdc
Vytautas Civilis <civilis@idega.com>
2009-02-26 08:52:29 +0000

    deprecation added

--- src/java/com/idega/jbpm/identity/permission/RoleScope.java
+++ src/java/com/idega/jbpm/identity/permission/RoleScope.java
@@ -1,11 +1,14 @@
 package com.idega.jbpm.identity.permission;
 
 /**
+ * @deprecated no more used, redundant
+ * 
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  *
- * Last modified: $Date: 2008/03/24 19:49:30 $ by $Author: civilis $
+ * Last modified: $Date: 2009/02/26 08:52:29 $ by $Author: civilis $
  */
+@Deprecated
 public enum RoleScope {
 
 	/**
