7185bc654e1321c354919096ebb35ceac7a05fd4
Vytautas Civilis <civilis@idega.com>
2009-03-27 15:33:14 +0000

    isHasViewUI method added

--- src/java/com/idega/jbpm/exe/BPMDocument.java
+++ src/java/com/idega/jbpm/exe/BPMDocument.java
@@ -3,33 +3,34 @@ package com.idega.jbpm.exe;
 import java.util.Date;
 
 public interface BPMDocument {
-
+	
 	public abstract String getSubmittedByName();
-
+	
 	public abstract void setSubmittedByName(String submittedByName);
-
+	
 	public abstract String getDocumentName();
-
+	
 	public abstract void setDocumentName(String documentName);
-
+	
 	public abstract boolean isSignable();
-
+	
 	public abstract void setSignable(boolean signable);
-
+	
 	public abstract Long getTaskInstanceId();
-
+	
 	public abstract void setTaskInstanceId(Long taskInstanceId);
-
+	
 	public abstract String getAssignedToName();
-
+	
 	public abstract void setAssignedToName(String assignedToName);
-
+	
 	public abstract Date getCreateDate();
-
+	
 	public abstract void setCreateDate(Date createDate);
-
+	
 	public abstract Date getEndDate();
-
+	
 	public abstract void setEndDate(Date endDate);
-
+	
+	public abstract boolean isHasViewUI();
 }
--- src/java/com/idega/jbpm/exe/impl/BPMDocumentImpl.java
+++ src/java/com/idega/jbpm/exe/impl/BPMDocumentImpl.java
@@ -5,19 +5,14 @@ import java.util.Date;
 import com.idega.jbpm.exe.BPMDocument;
 
 /**
- * Represents document in bpm - this can be representation of task, or document
- * (submitted task).
- * 
+ * Represents document in bpm - this can be representation of task, or document (submitted task).
  * Represents document in the locale it was resolved for
  * 
- * 
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.1 $
- * 
- *          Last modified: $Date: 2009/01/05 04:06:10 $ by $Author: juozas $
+ * @version $Revision: 1.2 $ Last modified: $Date: 2009/03/27 15:33:14 $ by $Author: civilis $
  */
 public class BPMDocumentImpl implements BPMDocument {
-
+	
 	private Long taskInstanceId;
 	private String submittedByName;
 	private String assignedToName;
@@ -25,102 +20,69 @@ public class BPMDocumentImpl implements BPMDocument {
 	private Date createDate;
 	private Date endDate;
 	private boolean signable;
-
-	/* (non-Javadoc)
-	 * @see com.idega.jbpm.exe.BPMDocument#getSubmittedByName()
-	 */
+	private boolean hasViewUI = true;
+	
 	public String getSubmittedByName() {
 		return submittedByName;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.idega.jbpm.exe.BPMDocument#setSubmittedByName(java.lang.String)
-	 */
+	
 	public void setSubmittedByName(String submittedByName) {
 		this.submittedByName = submittedByName;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.idega.jbpm.exe.BPMDocument#getDocumentName()
-	 */
+	
 	public String getDocumentName() {
 		return documentName;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.idega.jbpm.exe.BPMDocument#setDocumentName(java.lang.String)
-	 */
+	
 	public void setDocumentName(String documentName) {
 		this.documentName = documentName;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.idega.jbpm.exe.BPMDocument#isSignable()
-	 */
+	
 	public boolean isSignable() {
 		return signable;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.idega.jbpm.exe.BPMDocument#setSignable(boolean)
-	 */
+	
 	public void setSignable(boolean signable) {
 		this.signable = signable;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.idega.jbpm.exe.BPMDocument#getTaskInstanceId()
-	 */
+	
 	public Long getTaskInstanceId() {
 		return taskInstanceId;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.idega.jbpm.exe.BPMDocument#setTaskInstanceId(java.lang.Long)
-	 */
+	
 	public void setTaskInstanceId(Long taskInstanceId) {
 		this.taskInstanceId = taskInstanceId;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.idega.jbpm.exe.BPMDocument#getAssignedToName()
-	 */
+	
 	public String getAssignedToName() {
 		return assignedToName;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.idega.jbpm.exe.BPMDocument#setAssignedToName(java.lang.String)
-	 */
+	
 	public void setAssignedToName(String assignedToName) {
 		this.assignedToName = assignedToName;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.idega.jbpm.exe.BPMDocument#getCreateDate()
-	 */
+	
 	public Date getCreateDate() {
 		return createDate;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.idega.jbpm.exe.BPMDocument#setCreateDate(java.util.Date)
-	 */
+	
 	public void setCreateDate(Date createDate) {
 		this.createDate = createDate;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.idega.jbpm.exe.BPMDocument#getEndDate()
-	 */
+	
 	public Date getEndDate() {
 		return endDate;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.idega.jbpm.exe.BPMDocument#setEndDate(java.util.Date)
-	 */
+	
 	public void setEndDate(Date endDate) {
 		this.endDate = endDate;
 	}
+
+	public boolean isHasViewUI() {
+    	return hasViewUI;
+    }
+
+	public void setHasViewUI(boolean hasViewUI) {
+    	this.hasViewUI = hasViewUI;
+    }
 }
