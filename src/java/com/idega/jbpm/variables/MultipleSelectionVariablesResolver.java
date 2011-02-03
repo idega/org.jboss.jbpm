fc7119527d7451b34b34dc8748566864a43506df
Valdas Žemaitis <valdas@idega.com>
2011-02-03 13:10:50 +0200

    Default resolver for values of variables with multiple values

---
+++ src/java/com/idega/jbpm/variables/MultipleSelectionVariablesResolver.java
@@ -0,0 +1,160 @@
+package com.idega.jbpm.variables;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+import java.util.logging.Level;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.idega.builder.bean.AdvancedProperty;
+import com.idega.core.business.DefaultSpringBean;
+import com.idega.jbpm.bean.BPMProcessVariable;
+import com.idega.jbpm.bean.VariableInstanceInfo;
+import com.idega.jbpm.data.VariableInstanceQuerier;
+import com.idega.jbpm.data.dao.BPMDAO;
+import com.idega.jbpm.utils.JSONUtil;
+import com.idega.util.ListUtil;
+import com.idega.util.StringUtil;
+import com.idega.util.expression.ELUtil;
+
+public abstract class MultipleSelectionVariablesResolver extends DefaultSpringBean {
+
+	public static final String BEAN_NAME_PREFIX = "bpmVariableValueResolver";
+	
+	@Autowired
+	private VariableInstanceQuerier variablesQuerier;
+	
+	@Autowired
+	private BPMDAO bpmDAO;
+
+	private JSONUtil jsonUtil;
+	
+	protected Collection<AdvancedProperty> values;
+	
+	public abstract Collection<AdvancedProperty> getValues(String procDefId, String variableName);
+	
+	protected JSONUtil getJSONUtil() {
+		if (jsonUtil == null)
+			jsonUtil = new JSONUtil();
+		return jsonUtil;
+	}
+	
+	protected String getProcessDefNameByProcessDefId(String procDefId) {
+		if (StringUtil.isEmpty(procDefId))
+			return null;
+		
+		try {
+			return getBpmDAO().getProcessDefinitionNameByProcessDefinitionId(Long.valueOf(procDefId));
+		} catch (Exception e) {
+			getLogger().log(Level.WARNING, "Error getting process definition name by id: " + procDefId, e);
+		}
+		
+		return null;
+	}
+	
+	protected Collection<VariableInstanceInfo> getVariables(String procDefName, String variableName) {
+		if (StringUtil.isEmpty(procDefName) || StringUtil.isEmpty(variableName)) {
+			return null;
+		}
+		
+		Collection<Long> procInstIds = getBpmDAO().getProcessInstanceIdsByProcessDefinitionNames(Arrays.asList(procDefName));
+		if (ListUtil.isEmpty(procInstIds)) {
+			return null;
+		}
+		
+		return getVariablesQuerier().getVariablesByProcessInstanceIdAndVariablesNames(procInstIds, Arrays.asList(variableName), false);
+	}
+	
+	public Collection<AdvancedProperty> getBinaryVariablesValues(Collection<VariableInstanceInfo> vars) {
+		if (ListUtil.isEmpty(vars))
+			return null;
+		
+		Collection<AdvancedProperty> results = new ArrayList<AdvancedProperty>();
+		for (VariableInstanceInfo var: vars) {
+			Serializable value = var.getValue();
+			if (!(value instanceof Collection))
+				continue;
+			
+			Collection<AdvancedProperty> tmp = getValues((Collection<?>) value);
+			if (tmp != null)
+				results.addAll(tmp);
+		}
+		return results;
+	}
+	
+	protected Collection<AdvancedProperty> getValues(Collection<?> entries) {
+		if (ListUtil.isEmpty(entries))
+			return null;
+		
+		Collection<AdvancedProperty> results = new ArrayList<AdvancedProperty>();
+		for (Object entry: entries) {
+			if (entry == null)
+				continue;
+			
+			AdvancedProperty prop = getValueFromString(entry.toString());
+			if (prop != null)
+				results.add(prop);
+		}
+		return results;
+	}
+	
+	protected AdvancedProperty getValueFromString(String value) {
+		Object obj = null;
+		try {
+			obj = getJSONUtil().convertToObject(value);
+		} catch (Exception e) {
+			getLogger().log(Level.WARNING, "Error converting JSON string to object: " + value, e);
+		}
+		if (obj instanceof Map) {
+			Object id = ((Map<?, ?>) obj).get(getIdKey());
+			if (id instanceof String && !StringUtil.isEmpty((String) id)) {
+				Object realValue = ((Map<?, ?>) obj).get(getValueKey());
+				return new AdvancedProperty((String) id, (String) realValue);
+			}
+		}
+		return null;
+	}
+	
+	public abstract String getIdKey();
+	public abstract String getValueKey();
+	
+	protected VariableInstanceQuerier getVariablesQuerier() {
+		if (variablesQuerier == null)
+			ELUtil.getInstance().autowire(this);
+		
+		return variablesQuerier;
+	}
+
+	protected void setVariablesQuerier(VariableInstanceQuerier variablesQuerier) {
+		this.variablesQuerier = variablesQuerier;
+	}
+
+	protected BPMDAO getBpmDAO() {
+		if (bpmDAO == null)
+			ELUtil.getInstance().autowire(this);
+		
+		return bpmDAO;
+	}
+
+	protected void setBpmDAO(BPMDAO bpmDAO) {
+		this.bpmDAO = bpmDAO;
+	}
+	
+	protected void addEmptyLabel(String bundleIdentifier) {
+		if (values == null)
+			values = new ArrayList<AdvancedProperty>();
+		
+		values.add(new AdvancedProperty(String.valueOf(-1),
+				getResourceBundle(getBundle(bundleIdentifier)).getLocalizedString(getNoValuesLocalizationKey(), getNoValuesDefaultString())));
+	}
+	
+	protected abstract String getNoValuesLocalizationKey();
+	protected abstract String getNoValuesDefaultString();
+	
+	public abstract String getPresentation(BPMProcessVariable variable);
+	public abstract String getPresentation(String value);
+	public abstract String getPresentation(VariableInstanceInfo variable);
+}
