ae13097e3289c2d672e6260ec60a6c00882ae7ab
Valdas Žemaitis <valdas@idega.com>
2012-01-03 15:56:35 +0200

    New constant

--- src/java/com/idega/jbpm/utils/JBPMConstants.java
+++ src/java/com/idega/jbpm/utils/JBPMConstants.java
@@ -6,8 +6,8 @@ import com.idega.util.CoreConstants;
 
 public class JBPMConstants {
 
-	public static final String BPM_PATH = CoreConstants.PATH_FILES_ROOT
-			+ "/bpm";
+	public static final String	BPM_PATH = CoreConstants.PATH_FILES_ROOT + "/bpm",
+								VARIABLE_LOCALIZATION_PREFIX = "bpm_variable.";
 
 	public static final Logger bpmLogger = Logger.getLogger("IdegaBPMLogging");
 }
