372435156d95fcf4b02a23a887de6a9ea179e571
Valdas Žemaitis <valdas@idega.com>
2013-01-17 16:23:08 +0200

    Moved method to BPM Factory

--- src/java/com/idega/jbpm/exe/BPMFactory.java
+++ src/java/com/idega/jbpm/exe/BPMFactory.java
@@ -16,51 +16,51 @@ import com.idega.jbpm.view.ViewSubmission;
  * @version $Revision: 1.20 $ Last modified: $Date: 2009/03/20 19:17:17 $ by $Author: civilis $
  */
 public interface BPMFactory {
-	
+
 	/**
 	 * locates process definition name, and finds process manager Use this only if you don't have
 	 * process name
-	 * 
+	 *
 	 * @param processDefinitionId
 	 * @return Process manager bound to process definition
 	 */
 	public abstract ProcessManager getProcessManager(long processDefinitionId);
-	
+
 	/**
 	 * @param processDefinitionId
 	 * @return Process manager bound to latest process definition found by name
 	 */
 	public abstract ProcessManager getProcessManager(String processName);
-	
+
 	/**
 	 * @param processInstanceId
 	 * @return Process manager bound to process definition found by processInstanceId
 	 */
 	public abstract ProcessManager getProcessManagerByProcessInstanceId(
 	        long processInstanceId);
-	
+
 	/**
 	 * @param taskInstanceId
 	 * @return Process manager bound to process definition found by taskInstanceId
 	 */
 	public abstract ProcessManager getProcessManagerByTaskInstanceId(
 	        long taskInstanceId);
-	
+
 	/**
 	 * use when you don't have neither information (process id, process name and alike), but you
 	 * need general methods for processManager (e.g. getAllProcesses (for this process managers
 	 * type))
-	 * 
+	 *
 	 * @param managerType
 	 * @return
 	 */
 	public abstract ProcessManager getProcessManagerByType(String managerType);
-	
+
 	/**
 	 * Finds viewTaskBind by taskId provided, and finds ViewFactory by preferred types in order
 	 * given, if any provided Uses the ViewFactory resolved for creating View for view identifier
 	 * resolved in viewTaskBind
-	 * 
+	 *
 	 * @param taskId
 	 * @param submitable
 	 *            - should the view be able to be submitted (e.g. for html forms, the submit button
@@ -71,45 +71,45 @@ public interface BPMFactory {
 	 */
 	public abstract View getViewByTask(long taskId, boolean submitable,
 	        List<String> preferredTypes);
-	
+
 	public abstract View getViewByTaskInstance(long taskInstanceId,
 	        boolean submitable, List<String> preferredTypes);
-	
+
 	public abstract View getView(String viewIdentifier, String type,
 	        boolean submitable);
-	
+
 	public abstract View takeView(long taskInstanceId, boolean submitable,
 	        List<String> preferredTypes);
-	
+
 	/**
 	 * takes all views that are bound to the task instance task, if not taken already
-	 * 
+	 *
 	 * @param taskInstanceId
 	 */
 	public abstract void takeViews(final long taskInstanceId);
-	
+
 	public abstract RolesManager getRolesManager();
-	
+
 	public abstract BPMUserFactory getBpmUserFactory();
-	
+
 	/**
 	 * returns default view submission implementation
-	 * 
+	 *
 	 * @return
 	 */
 	public abstract ViewSubmission getViewSubmission();
-	
+
 	public abstract BPMDAO getBPMDAO();
-	
+
 	public abstract PermissionsFactory getPermissionsFactory();
-	
+
 	/**
 	 * Resolves mainProcessInstance in this manner: if provided processInstance contains variable
 	 * "mainProcessInstanceId", then the processInstance from this variable value is used. Else,
 	 * super process instances are checked for this variable. If the variable is not found in super
 	 * processes, then the most super process instance is used - that means the process instance
 	 * without the parent
-	 * 
+	 *
 	 * @param processInstanceId
 	 *            of any process instance (subprocess or not)
 	 * @return the main process instance, which reflects the most parent process instance. This
@@ -117,10 +117,12 @@ public interface BPMFactory {
 	 */
 	public abstract ProcessInstance getMainProcessInstance(
 	        final long processInstanceId);
-	
+
 	public abstract TaskInstanceW getTaskInstanceW(long taskInstanceId);
-	
+
 	public abstract ProcessInstanceW getProcessInstanceW(long processInstanceId);
-	
+
 	public abstract ProcessDefinitionW getProcessDefinitionW(String processName);
+
+	public Long getIdOfStartTaskInstance(Long piId);
 }
--- src/java/com/idega/jbpm/exe/impl/BPMFactoryImpl.java
+++ src/java/com/idega/jbpm/exe/impl/BPMFactoryImpl.java
@@ -1,5 +1,6 @@
 package com.idega.jbpm.exe.impl;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -15,10 +16,12 @@ import org.jbpm.graph.exe.ProcessInstance;
 import org.jbpm.graph.exe.Token;
 import org.jbpm.taskmgmt.exe.TaskInstance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.idega.data.SimpleQuerier;
 import com.idega.jbpm.BPMContext;
 import com.idega.jbpm.JbpmCallback;
 import com.idega.jbpm.data.ProcessManagerBind;
@@ -37,19 +40,22 @@ import com.idega.jbpm.view.View;
 import com.idega.jbpm.view.ViewFactory;
 import com.idega.jbpm.view.ViewSubmission;
 import com.idega.jbpm.view.ViewSubmissionImpl;
+import com.idega.util.ArrayUtil;
 import com.idega.util.ListUtil;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
  * @version $Revision: 1.29 $ Last modified: $Date: 2009/03/20 19:18:18 $ by $Author: civilis $
  */
-@Scope("singleton")
+@Scope(BeanDefinition.SCOPE_SINGLETON)
 @Service("bpmFactory")
 public class BPMFactoryImpl implements BPMFactory {
-	
+
+	private static final Logger LOGGER = Logger.getLogger(BPMFactoryImpl.class.getName());
+
 	private Map<String, ProcessManager> processManagers;
 	private Map<String, ViewFactory> viewFactories;
-	
+
 	@Autowired
 	private BPMDAO bpmDAO;
 	@Autowired
@@ -60,152 +66,161 @@ public class BPMFactoryImpl implements BPMFactory {
 	private BPMUserFactory bpmUserFactory;
 	@Autowired
 	private PermissionsFactory permissionsFactory;
-	
+
+	@Override
 	@Transactional(readOnly = true)
 	public ProcessManager getProcessManager(final long processDefinitionId) {
-		
+
 		return getBpmContext().execute(new JbpmCallback() {
-			
+
+			@Override
 			public Object doInJbpm(JbpmContext context) throws JbpmException {
-				
+
 				ProcessDefinition pd = context.getGraphSession()
 				        .getProcessDefinition(processDefinitionId);
 				return getProcessManager(pd.getName());
 			}
 		});
 	}
-	
+
+	@Override
 	@Transactional(readOnly = false)
 	public View takeView(final long taskInstanceId, final boolean submitable,
 	        final List<String> preferredTypes) {
-		
+
 		return getBpmContext().execute(new JbpmCallback() {
-			
+
+			@Override
 			public Object doInJbpm(JbpmContext context) throws JbpmException {
-				
+
 				TaskInstance ti = context.getTaskInstance(taskInstanceId);
-				
+
 				View view = getViewByTask(ti.getTask().getId(), submitable,
 				    preferredTypes);
-				
+
 				if (view != null) {
-					
+
 					takeView(view, ti);
 				}
-				
+
 				return view;
 			}
 		});
 	}
-	
+
 	protected void takeView(View view, TaskInstance ti) {
-		
+
 		if (!view.getViewToTask().containsBind(view.getViewType(), ti.getId())) {
-			
+
 			view.takeView();
 			view.getViewToTask().bind(view, ti);
 		} else {
-			
+
 		}
 	}
-	
+
+	@Override
 	@Transactional(readOnly = false)
 	public void takeViews(final long taskInstanceId) {
-		
+
 		getBpmContext().execute(new JbpmCallback() {
-			
+
+			@Override
 			public Object doInJbpm(JbpmContext context) throws JbpmException {
-				
+
 				TaskInstance ti = context.getTaskInstance(taskInstanceId);
-				
+
 				List<View> views = getViewsByTask(ti.getTask().getId(), false);
-				
+
 				for (View view : views) {
-					
+
 					takeView(view, ti);
 				}
-				
+
 				return null;
 			}
 		});
 	}
-	
+
+	@Override
 	@Transactional(readOnly = true)
 	public View getViewByTask(long taskId, boolean submitable,
 	        List<String> preferredTypes) {
-		
+
 		List<ViewTaskBind> binds = getBPMDAO().getViewTaskBindsByTaskId(taskId);
-		
+
 		if (binds == null || binds.isEmpty()) {
 			Logger.getLogger(BPMFactory.class.getName()).log(Level.WARNING,
 			    "No view task bindings resolved for task. Task id: " + taskId);
 			return null;
 		}
-		
+
 		ViewTaskBind viewTaskBind = getPreferredViewTaskBind(binds,
 		    preferredTypes);
 		String viewType = viewTaskBind.getViewType();
-		
+
 		ViewFactory viewFactory = getViewFactory(viewType);
 		return viewFactory
 		        .getView(viewTaskBind.getViewIdentifier(), submitable);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public List<View> getViewsByTask(long taskId, boolean submitable) {
-		
+
 		List<ViewTaskBind> binds = getBPMDAO().getViewTaskBindsByTaskId(taskId);
-		
+
 		if (ListUtil.isEmpty(binds)) {
 			Logger.getLogger(BPMFactory.class.getName()).log(Level.WARNING,
 			    "No view task bindings resolved for task. Task id: " + taskId);
 			return Collections.emptyList();
 		}
-		
+
 		ArrayList<View> views = new ArrayList<View>(binds.size());
-		
+
 		for (ViewTaskBind viewTaskBind : binds) {
-			
+
 			String viewType = viewTaskBind.getViewType();
-			
+
 			ViewFactory viewFactory = getViewFactory(viewType);
 			View view = viewFactory.getView(viewTaskBind.getViewIdentifier(),
 			    submitable);
-			
+
 			views.add(view);
 		}
-		
+
 		return views;
 	}
-	
+
+	@Override
 	@Transactional(readOnly = true)
 	public View getView(String viewIdentifier, String type, boolean submitable) {
-		
+
 		ViewFactory viewFactory = getViewFactory(type);
 		return viewFactory.getView(viewIdentifier, submitable);
 	}
-	
+
+	@Override
 	@Transactional(readOnly = true)
 	public View getViewByTaskInstance(long taskInstanceId, boolean submitable,
 	        List<String> preferredTypes) {
-		
+
 		// TODO: if not found - take from task
-		
+
 		List<ViewTaskBind> binds = getBPMDAO()
 		        .getViewTaskBindsByTaskInstanceId(taskInstanceId);
-		
+
 		if (ListUtil.isEmpty(binds)) {
-			
+
 			// no view taken - this is probably backward compatibility. In new manner, the view is
 			// taken at TaskInstanceViewBindHandler, which is launched on task-create event of the
 			// process (see configure method of ProcessBundleSingleProcessDefinitionImpl)
-			
+
 			takeViews(taskInstanceId);
-			
+
 			binds = getBPMDAO()
 			        .getViewTaskBindsByTaskInstanceId(taskInstanceId);
 		}
-		
+
 		if (binds == null || binds.isEmpty()) {
 			Logger.getLogger(BPMFactory.class.getName()).log(
 			    Level.WARNING,
@@ -213,28 +228,28 @@ public class BPMFactoryImpl implements BPMFactory {
 			            + taskInstanceId);
 			return null;
 		}
-		
+
 		ViewTaskBind viewTaskBind = getPreferredViewTaskBind(binds,
 		    preferredTypes);
 		String viewType = viewTaskBind.getViewType();
-		
+
 		ViewFactory viewFactory = getViewFactory(viewType);
 		return viewFactory
 		        .getView(viewTaskBind.getViewIdentifier(), submitable);
 	}
-	
+
 	@Transactional(readOnly = true)
 	protected ViewTaskBind getPreferredViewTaskBind(List<ViewTaskBind> binds,
 	        List<String> preferredTypes) {
-		
+
 		ViewTaskBind viewTaskBind = null;
-		
+
 		if (preferredTypes != null) {
-			
+
 			for (String preferredType : preferredTypes) {
-				
+
 				for (ViewTaskBind bind : binds) {
-					
+
 					if (preferredType.equals(bind.getViewType())) {
 						viewTaskBind = bind;
 						break;
@@ -242,66 +257,69 @@ public class BPMFactoryImpl implements BPMFactory {
 				}
 			}
 		}
-		
+
 		if (viewTaskBind == null && !binds.isEmpty())
 			viewTaskBind = binds.get(0);
-		
+
 		return viewTaskBind;
 	}
-	
+
 	@Transactional(readOnly = true)
 	protected ViewFactory getViewFactory(String viewType) {
-		
+
 		ViewFactory viewFactory;
-		
+
 		if (getViewFactories().containsKey(viewType)) {
 			viewFactory = getViewFactories().get(viewType);
-			
+
 		} else {
 			throw new IllegalStateException(
 			        "No View Factory registered for view type: " + viewType);
 		}
-		
+
 		return viewFactory;
 	}
-	
+
 	@Transactional(readOnly = true)
 	protected String resolveManagersType(String processName) {
-		
+
 		ProcessManagerBind pm = getBPMDAO().getProcessManagerBind(processName);
-		
+
 		if (pm == null) {
-			
+
 			return "default";
 		}
-		
+
 		return pm.getManagersType();
 	}
-	
+
 	@Autowired(required = false)
 	public void setProcessManagers(List<ProcessManager> processManagersList) {
-		
+
 		// hashmap is thread safe for read only ops
 		processManagers = new HashMap<String, ProcessManager>(
 		        processManagersList.size());
-		
+
 		for (ProcessManager processManager : processManagersList) {
-			
+
 			processManagers
 			        .put(processManager.getManagerType(), processManager);
 		}
 	}
-	
+
+	@Override
 	public BPMDAO getBPMDAO() {
 		return bpmDAO;
 	}
-	
+
+	@Override
 	@Transactional(readOnly = true)
 	public ProcessManager getProcessManagerByTaskInstanceId(
 	        final long taskInstanceId) {
-		
+
 		return getBpmContext().execute(new JbpmCallback() {
-			
+
+			@Override
 			public Object doInJbpm(JbpmContext context) throws JbpmException {
 				TaskInstance taskInstance = context
 				        .getTaskInstance(taskInstanceId);
@@ -311,58 +329,65 @@ public class BPMFactoryImpl implements BPMFactory {
 			}
 		});
 	}
-	
+
+	@Override
 	public TaskInstanceW getTaskInstanceW(long taskInstanceId) {
-		
+
 		return getProcessManagerByTaskInstanceId(taskInstanceId)
 		        .getTaskInstance(taskInstanceId);
 	}
-	
+
+	@Override
 	public ProcessInstanceW getProcessInstanceW(long processInstanceId) {
-		
+
 		return getProcessManagerByProcessInstanceId(processInstanceId)
 		        .getProcessInstance(processInstanceId);
 	}
-	
+
+	@Override
 	public ProcessDefinitionW getProcessDefinitionW(String processName) {
-		
+
 		return getProcessManager(processName).getProcessDefinition(processName);
 	}
-	
+
 	public BPMContext getBpmContext() {
 		return bpmContext;
 	}
-	
+
+	@Override
 	public RolesManager getRolesManager() {
 		return rolesManager;
 	}
-	
+
 	public void setBPMDAO(BPMDAO bpmDAO) {
 		this.bpmDAO = bpmDAO;
 	}
-	
+
 	public void setBpmContext(BPMContext bpmContext) {
 		this.bpmContext = bpmContext;
 	}
-	
+
 	public void setRolesManager(RolesManager rolesManager) {
 		this.rolesManager = rolesManager;
 	}
-	
+
+	@Override
 	public BPMUserFactory getBpmUserFactory() {
 		return bpmUserFactory;
 	}
-	
+
 	public void setBpmUserFactory(BPMUserFactory bpmUserFactory) {
 		this.bpmUserFactory = bpmUserFactory;
 	}
-	
+
+	@Override
 	@Transactional(readOnly = true)
 	public ProcessManager getProcessManagerByProcessInstanceId(
 	        final long processInstanceId) {
-		
+
 		return getBpmContext().execute(new JbpmCallback() {
-			
+
+			@Override
 			public Object doInJbpm(JbpmContext context) throws JbpmException {
 				ProcessInstance processInstance = context
 				        .getProcessInstance(processInstanceId);
@@ -371,111 +396,145 @@ public class BPMFactoryImpl implements BPMFactory {
 			}
 		});
 	}
-	
+
+	@Override
 	public ProcessManager getProcessManagerByType(String managerType) {
-		
+
 		return getProcessManagers().get(managerType);
 	}
-	
+
+	@Override
 	public ProcessManager getProcessManager(String processName) {
-		
+
 		String managerType = resolveManagersType(processName);
 		return getProcessManagerByType(managerType);
 	}
-	
+
+	@Override
 	public ViewSubmission getViewSubmission() {
 		return new ViewSubmissionImpl();
 	}
-	
+
+	@Override
 	public PermissionsFactory getPermissionsFactory() {
 		return permissionsFactory;
 	}
-	
+
 	public Map<String, ProcessManager> getProcessManagers() {
-		
+
 		if (processManagers == null)
 			processManagers = Collections.emptyMap();
-		
+
 		return processManagers;
 	}
-	
+
 	public Map<String, ViewFactory> getViewFactories() {
-		
+
 		if (viewFactories == null)
 			viewFactories = Collections.emptyMap();
-		
+
 		return viewFactories;
 	}
-	
+
 	@Autowired(required = false)
 	public void setViewFactories(List<ViewFactory> viewFactoriesList) {
-		
+
 		// hashmap is thread safe for read only ops
 		viewFactories = new HashMap<String, ViewFactory>(viewFactoriesList
 		        .size());
-		
+
 		for (ViewFactory viewFactory : viewFactoriesList) {
-			
+
 			viewFactories.put(viewFactory.getViewType(), viewFactory);
 		}
 	}
-	
+
+	@Override
 	public ProcessInstance getMainProcessInstance(final long processInstanceId) {
-		
+
 		return getBpmContext().execute(new JbpmCallback() {
-			
+
+			@Override
 			public Object doInJbpm(JbpmContext context) throws JbpmException {
-				
+
 				ProcessInstance pi = context
 				        .getProcessInstance(processInstanceId);
 				Long mainProcessInstanceId = (Long) pi.getContextInstance()
 				        .getVariable(
 				            ProcessConstants.mainProcessInstanceIdVariableName);
-				
+
 				ProcessInstance mainProcessInstance;
-				
+
 				if (mainProcessInstanceId != null) {
-					
+
 					// if mainProcessInstanceId found in variable - use that
-					
+
 					mainProcessInstance = context
 					        .getProcessInstance(mainProcessInstanceId);
-					
+
 				} else {
-					
+
 					// if mainProcessInstanceId not found in variable - search
 					// super processes. If we find mainProcessInstanceId in any
 					// of the super process
 					// we use that, else, use most super processInstance (one
 					// without parent)
-					
+
 					Token superToken = pi.getSuperProcessToken();
-					
+
 					while (superToken != null && mainProcessInstanceId == null) {
-						
+
 						pi = superToken.getProcessInstance();
-						
+
 						mainProcessInstanceId = (Long) pi
 						        .getContextInstance()
 						        .getVariable(
 						            ProcessConstants.mainProcessInstanceIdVariableName);
-						
+
 						if (mainProcessInstanceId == null) {
-							
+
 							superToken = pi.getSuperProcessToken();
 						}
 					}
-					
+
 					if (mainProcessInstanceId != null) {
-						
+
 						mainProcessInstance = context
 						        .getProcessInstance(mainProcessInstanceId);
 					} else
 						mainProcessInstance = pi;
 				}
-				
+
 				return mainProcessInstance;
 			}
 		});
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Long getIdOfStartTaskInstance(Long piId) {
+		Long id = null;
+		String query = "select t.id_ from jbpm_taskinstance t, jbpm_processinstance p, jbpm_processdefinition d, JBPM_MODULEDEFINITION m " +
+				"where p.id_ = " + piId + " and t.PROCINST_ = p.id_ and p.processdefinition_ = d.id_ and " +
+				"m.PROCESSDEFINITION_ = d.id_ and m.STARTTASK_ = t.TASK_";
+		try {
+			List<Serializable[]> result = SimpleQuerier.executeQuery(query, 1);
+			if (!ListUtil.isEmpty(result)) {
+				Serializable[] data = result.iterator().next();
+				if (!ArrayUtil.isEmpty(data)) {
+					Serializable startId = data[0];
+					if (startId instanceof Number)
+						id = ((Number) startId).longValue();
+				}
+			}
+		} catch (Exception e) {
+			LOGGER.log(Level.WARNING, "Error executing query: " + query, e);
+		}
+
+		if (id != null)
+			return id;
+
+		return getProcessManagerByProcessInstanceId(piId).getProcessInstance(piId).getProcessDefinitionW()
+				.getProcessDefinition().getTaskMgmtDefinition().getStartTask().getId();
 	}
 }
