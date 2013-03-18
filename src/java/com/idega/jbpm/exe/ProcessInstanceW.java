82a1ec8a66ff9d626275a3fe37e4f7596792fd89
Valdas Žemaitis <valdas@idega.com>
2013-03-18 12:46:33 +0200

    Layout changes

--- src/java/com/idega/jbpm/exe/ProcessInstanceW.java
+++ src/java/com/idega/jbpm/exe/ProcessInstanceW.java
@@ -34,13 +34,13 @@ public interface ProcessInstanceW {
 
 	public abstract List<BPMDocument> getSubmittedDocumentsForUser(User user,
 	        Locale locale);
-	
+
 	public abstract List<BPMDocument> getSubmittedDocumentsForUser(User user,
 	        Locale locale, boolean doShowExternalEntity);
 
 	public abstract List<BPMDocument> getTaskDocumentsForUser(User user,
 	        Locale locale);
-	
+
 	public abstract List<BPMDocument> getTaskDocumentsForUser(User user,
 	        Locale locale, boolean doShowExternalEntity);
 
