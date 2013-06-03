f614a2d84dd006c4c743bbbbbaf9f691f0502f38
lapiukshtiss <martynas.stake@gmail.com>
2013-06-03 14:32:12 +0300

    Service providing variable value by view

---
+++ src/java/com/idega/jbpm/business/impl/BPMVariableServiceImpl.java
@@ -0,0 +1,209 @@
+/**
+ * @(#)BPMVariableServiceImpl.java    1.0.0 2:05:14 PM
+ *
+ * Idega Software hf. Source Code Licence Agreement x
+ *
+ * This agreement, made this 10th of February 2006 by and between 
+ * Idega Software hf., a business formed and operating under laws 
+ * of Iceland, having its principal place of business in Reykjavik, 
+ * Iceland, hereinafter after referred to as "Manufacturer" and Agura 
+ * IT hereinafter referred to as "Licensee".
+ * 1.  License Grant: Upon completion of this agreement, the source 
+ *     code that may be made available according to the documentation for 
+ *     a particular software product (Software) from Manufacturer 
+ *     (Source Code) shall be provided to Licensee, provided that 
+ *     (1) funds have been received for payment of the License for Software and 
+ *     (2) the appropriate License has been purchased as stated in the 
+ *     documentation for Software. As used in this License Agreement, 
+ *     Licensee shall also mean the individual using or installing 
+ *     the source code together with any individual or entity, including 
+ *     but not limited to your employer, on whose behalf you are acting 
+ *     in using or installing the Source Code. By completing this agreement, 
+ *     Licensee agrees to be bound by the terms and conditions of this Source 
+ *     Code License Agreement. This Source Code License Agreement shall 
+ *     be an extension of the Software License Agreement for the associated 
+ *     product. No additional amendment or modification shall be made 
+ *     to this Agreement except in writing signed by Licensee and 
+ *     Manufacturer. This Agreement is effective indefinitely and once
+ *     completed, cannot be terminated. Manufacturer hereby grants to 
+ *     Licensee a non-transferable, worldwide license during the term of 
+ *     this Agreement to use the Source Code for the associated product 
+ *     purchased. In the event the Software License Agreement to the 
+ *     associated product is terminated; (1) Licensee's rights to use 
+ *     the Source Code are revoked and (2) Licensee shall destroy all 
+ *     copies of the Source Code including any Source Code used in 
+ *     Licensee's applications.
+ * 2.  License Limitations
+ *     2.1 Licensee may not resell, rent, lease or distribute the 
+ *         Source Code alone, it shall only be distributed as a 
+ *         compiled component of an application.
+ *     2.2 Licensee shall protect and keep secure all Source Code 
+ *         provided by this this Source Code License Agreement. 
+ *         All Source Code provided by this Agreement that is used 
+ *         with an application that is distributed or accessible outside
+ *         Licensee's organization (including use from the Internet), 
+ *         must be protected to the extent that it cannot be easily 
+ *         extracted or decompiled.
+ *     2.3 The Licensee shall not resell, rent, lease or distribute 
+ *         the products created from the Source Code in any way that 
+ *         would compete with Idega Software.
+ *     2.4 Manufacturer's copyright notices may not be removed from 
+ *         the Source Code.
+ *     2.5 All modifications on the source code by Licencee must 
+ *         be submitted to or provided to Manufacturer.
+ * 3.  Copyright: Manufacturer's source code is copyrighted and contains 
+ *     proprietary information. Licensee shall not distribute or 
+ *     reveal the Source Code to anyone other than the software 
+ *     developers of Licensee's organization. Licensee may be held 
+ *     legally responsible for any infringement of intellectual property 
+ *     rights that is caused or encouraged by Licensee's failure to abide 
+ *     by the terms of this Agreement. Licensee may make copies of the 
+ *     Source Code provided the copyright and trademark notices are 
+ *     reproduced in their entirety on the copy. Manufacturer reserves 
+ *     all rights not specifically granted to Licensee.
+ *
+ * 4.  Warranty & Risks: Although efforts have been made to assure that the 
+ *     Source Code is correct, reliable, date compliant, and technically 
+ *     accurate, the Source Code is licensed to Licensee as is and without 
+ *     warranties as to performance of merchantability, fitness for a 
+ *     particular purpose or use, or any other warranties whether 
+ *     expressed or implied. Licensee's organization and all users 
+ *     of the source code assume all risks when using it. The manufacturers, 
+ *     distributors and resellers of the Source Code shall not be liable 
+ *     for any consequential, incidental, punitive or special damages 
+ *     arising out of the use of or inability to use the source code or 
+ *     the provision of or failure to provide support services, even if we 
+ *     have been advised of the possibility of such damages. In any case, 
+ *     the entire liability under any provision of this agreement shall be 
+ *     limited to the greater of the amount actually paid by Licensee for the 
+ *     Software or 5.00 USD. No returns will be provided for the associated 
+ *     License that was purchased to become eligible to receive the Source 
+ *     Code after Licensee receives the source code. 
+ */
+package com.idega.jbpm.business.impl;
+
+import java.math.BigDecimal;
+import java.util.logging.Level;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import com.idega.core.business.DefaultSpringBean;
+import com.idega.data.SimpleQuerier;
+import com.idega.jbpm.business.BPMVariableService;
+import com.idega.jbpm.variables.VariablesHandler;
+import com.idega.util.ArrayUtil;
+import com.idega.util.CoreConstants;
+import com.idega.util.StringUtil;
+import com.idega.util.expression.ELUtil;
+
+/**
+ * @see BPMVariableService
+ * <p>You can report about problems to: 
+ * <a href="mailto:martynas@idega.is">Martynas Stakė</a></p>
+ *
+ * @version 1.0.0 May 30, 2013
+ * @author <a href="mailto:martynas@idega.is">Martynas Stakė</a>
+ */
+@Service(BPMVariableService.BEAN_NAME)
+@Scope(BeanDefinition.SCOPE_SINGLETON)
+public class BPMVariableServiceImpl extends DefaultSpringBean implements BPMVariableService {
+	
+	@Autowired
+	private VariablesHandler variablesHandler;
+	
+	protected VariablesHandler getVariablesHandler() {
+		if (this.variablesHandler == null) {
+			ELUtil.getInstance().autowire(this);
+		}
+		
+		return this.variablesHandler;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.jbpm.business.BPMVariableService#getLastestVariable(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public String getLastestStringVariable(String processInstanceId, String variableName) {
+		if (StringUtil.isEmpty(processInstanceId) || StringUtil.isEmpty(variableName)) {
+			return null;
+		}
+
+		StringBuilder query = new StringBuilder("SELECT j.stringvalue_ FROM ");
+		query.append("jbpm_variableinstance WHERE j.PROCESSINSTANCE_='")
+		.append(processInstanceId).append("' AND j.NAME_ = '")
+		.append(variableName)
+		.append("' AND j.TASKINSTANCE_ IS NOT NULL ")
+		.append("ORDER BY j.TASKINSTANCE_ DESC LIMIT 1;");
+		
+		String[] results = null;
+		try {
+			results = SimpleQuerier.executeStringQuery(query.toString());
+		} catch (Exception e) {
+			getLogger().log(Level.WARNING, 
+					"Unable to find value of variable: " + variableName, e);
+		}
+		
+		if (results == null) {
+			return null;
+		}
+		
+		return results[0];
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.idega.jbpm.business.BPMVariableService#getLastestStringVariableByFormId(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public String getLastestStringVariableByFormId(String formId,
+			String variableName) {
+		if (StringUtil.isEmpty(formId) || StringUtil.isEmpty(variableName)) {
+			return null;
+		}
+
+		StringBuilder sb = new StringBuilder("SELECT jv.stringvalue_ ");
+		sb.append("FROM jbpm_variableinstance AS jv ")
+		.append("INNER JOIN jbpm_taskinstance AS j ")
+		.append("ON j.PROCINST_ = jv.PROCESSINSTANCE_ ")
+		.append("INNER JOIN bpm_views_tasks AS b ")
+		.append("ON j.ID_ = b.task_instance_id ")
+		.append("OR j.TASK_ = b.task_id ")
+		.append("WHERE b.view_identifier = ").append(formId).append(CoreConstants.SPACE)
+		.append("AND jv.NAME_ = '").append(variableName).append("' ")
+		.append("AND jv.TASKINSTANCE_ IS NOT NULL ")
+		.append("AND jv.stringvalue_ IS NOT NULL ")
+		.append("ORDER BY j.ID_ DESC;");
+
+		String[] values = null;
+		try {
+			values = SimpleQuerier.executeStringQuery(sb.toString());
+		} catch (Exception e) {
+			getLogger().log(Level.WARNING, "" +
+					"Unable to get variable " + variableName + 
+					" by form id " + formId + " cause of: ", e);
+		}
+		
+		String parsedValue = null;
+		if (!ArrayUtil.isEmpty(values)) {
+			parsedValue = values[0];
+			if (!StringUtil.isEmpty(parsedValue)) {
+				parsedValue = parsedValue.replace(CoreConstants.HASH, CoreConstants.EMPTY);
+				
+				BigDecimal decimal = null;
+				try {
+					decimal = new BigDecimal(parsedValue);
+					decimal = decimal.multiply(new BigDecimal(1000));
+					return decimal.toPlainString();
+				} catch (NumberFormatException e) {	
+					getLogger().log(Level.WARNING, 
+							"Failed to convert " + parsedValue + " to " + Long.class);
+				}
+			}
+		}
+		
+		return null;
+	}
+}
