f3b14521b04563e87dbd6fbb46f5cd903b89de3c
Valdas Žemaitis <valdas@idega.com>
2014-01-02 15:41:54 +0200

    Making sure session is available when doing lazy load

--- src/java/com/idega/jbpm/event/FireEventAspect.java
+++ src/java/com/idega/jbpm/event/FireEventAspect.java
@@ -58,20 +58,21 @@ public class FireEventAspect {
 
 					@Override
 					public Void doInJbpm(JbpmContext context) throws JbpmException {
-						TaskInstance taskInstance = ((TaskInstanceW) jpThis).getTaskInstance();
-						taskInstance = context.getTaskInstance(taskInstance.getId());
+						TaskInstance taskInstance = context.getTaskInstance(((TaskInstanceW) jpThis).getTaskInstanceId());
 						Task startTask = taskInstance.getTaskMgmtInstance().getTaskMgmtDefinition().getStartTask();
 
 						if (!taskInstance.getTask().equals(startTask)) {
 							//	Firing event only for task submit for non start task submit
 							final Token tkn = taskInstance.getToken();
-							fireEvent(tkn);
+							fireEvent(tkn, context);
 						}
 						return null;
 					}
 				});
-			} else
-				throw new IllegalArgumentException("Only objects of "+TaskInstanceW.class.getName()+" supported, got: "+jpThis.getClass().getName());
+			} else {
+				throw new IllegalArgumentException("Only objects of " + TaskInstanceW.class.getName() + " supported, got: " +
+						jpThis.getClass().getName());
+			}
 		} catch (Exception e) {
 			LOGGER.log(Level.SEVERE, "Exception while firing event", e);
 		}
@@ -85,51 +86,51 @@ public class FireEventAspect {
 	@AfterReturning("("+BPMPointcuts.setContactsPermissionAtProcessInstanceW+" || "+BPMPointcuts.setTaskRolePermissionsAtTaskInstanceW+")")
 	public void firePostStartEventOnPermissionChange(JoinPoint jp) {
 		try {
-			Object jpThis = jp.getThis();
+			final Object jpThis = jp.getThis();
 
-			Token tkn = null;
-			if (jpThis instanceof TaskInstanceW) {
-				tkn = ((TaskInstanceW)jpThis).getTaskInstance().getToken();
-				fireEvent(tkn);
-			} else if (jpThis instanceof ProcessInstanceW) {
-				fireEvent((ProcessInstanceW) jpThis);
-			} else
-				throw new IllegalArgumentException("Only objects of " + TaskInstanceW.class.getName() + " and " + ProcessInstanceW.class.getName() +
-						" supported, got: " + jpThis.getClass().getName());
+			getIdegaJbpmContext().execute(new JbpmCallback<Void>() {
+				@Override
+				public Void doInJbpm(JbpmContext context) throws JbpmException {
+					Token tkn = null;
+					if (jpThis instanceof TaskInstanceW) {
+						TaskInstance taskInst = context.getTaskInstance(((TaskInstanceW) jpThis).getTaskInstanceId());
+						tkn = taskInst.getToken();
+						fireEvent(tkn, context);
+
+					} else if (jpThis instanceof ProcessInstanceW) {
+						fireEvent((ProcessInstanceW) jpThis, context);
+
+					} else {
+						throw new IllegalArgumentException("Only objects of " + TaskInstanceW.class.getName() + " and " +
+								ProcessInstanceW.class.getName() + " supported, got: " + jpThis.getClass().getName());
+					}
+					return null;
+				}
+			});
 		} catch (Exception e) {
 			LOGGER.log(Level.SEVERE, "Exception while firing event", e);
 		}
 	}
 
-	private Boolean fireEvent(final ProcessInstanceW piw) {
-		return getIdegaJbpmContext().execute(new JbpmCallback<Boolean>() {
-			@Override
-			public Boolean doInJbpm(JbpmContext context) throws JbpmException {
-				Token tkn = piw.getProcessInstance(context).getRootToken();
-				ProcessDefinition pd = tkn.getProcessInstance().getProcessDefinition();
-				return fireEvent(tkn, pd);
-			}
-		});
+	private Boolean fireEvent(final ProcessInstanceW piw, JbpmContext context) {
+		Token tkn = piw.getProcessInstance(context).getRootToken();
+		ProcessDefinition pd = tkn.getProcessInstance().getProcessDefinition();
+		return fireEvent(tkn, pd, context);
 	}
 
-	private Boolean fireEvent(final Token token) {
-		return fireEvent(token, null);
+	private Boolean fireEvent(final Token token, JbpmContext context) {
+		return fireEvent(token, null, context);
 	}
 
-	private Boolean fireEvent(final Token token, final ProcessDefinition procDef) {
-		return getIdegaJbpmContext().execute(new JbpmCallback<Boolean>() {
-			@Override
-			public Boolean doInJbpm(JbpmContext context) throws JbpmException {
-				ProcessDefinition pd = procDef;
-				if (pd == null) {
-					pd = token.getProcessInstance().getProcessDefinition();
-				}
+	private Boolean fireEvent(final Token token, final ProcessDefinition procDef, JbpmContext context) {
+		ProcessDefinition pd = procDef;
+		if (pd == null) {
+			pd = token.getProcessInstance().getProcessDefinition();
+		}
 
-				ExecutionContext ectx = new ExecutionContext(token);
-				pd.fireEvent(eventType, ectx);
-				return Boolean.TRUE;
-			}
-		});
+		ExecutionContext ectx = new ExecutionContext(token);
+		pd.fireEvent(eventType, ectx);
+		return Boolean.TRUE;
 	}
 
 	public BPMContext getIdegaJbpmContext() {
