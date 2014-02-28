565a5995028b7d49eb3fa08747f82d8a8fd1eb72
Valdas Žemaitis <valdas@idega.com>
2014-02-28 09:09:31 +0200

    New method to get IDs of finished task instances by task name

--- src/java/com/idega/jbpm/data/dao/BPMDAO.java
+++ src/java/com/idega/jbpm/data/dao/BPMDAO.java
@@ -136,4 +136,7 @@ public interface BPMDAO extends GenericDao {
 	public List<Token> getProcessTokens(Long piId);
 
 	public List<Long> getSubProcInstIdsByParentProcInstIdAndProcDefName(Long piId, String procDefName);
+
+	public List<Long> getIdsOfFinishedTaskInstancesForTask(Long piId, String taskName);
+
 }
