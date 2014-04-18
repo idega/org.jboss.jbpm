c78ac9c2d927bea6afcb32b0ba7e9f5cfeca27d2
Valdas Žemaitis <valdas@idega.com>
2014-04-18 13:42:08 +0200

    New constants

--- src/java/com/idega/jbpm/process/business/messages/TypeRef.java
+++ src/java/com/idega/jbpm/process/business/messages/TypeRef.java
@@ -5,42 +5,45 @@ import com.idega.util.CoreConstants;
 /**
  * represents message value key. The main responsibility is to provide ala namespace for values in
  * the message context. The namespace can, but not need to be used by different handlers
- * 
+ *
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
  * @version $Revision: 1.2 $ Last modified: $Date: 2009/03/02 15:33:20 $ by $Author: civilis $
  */
 public final class TypeRef {
-	
+
+	public static final TypeRef CREATION_DATE = new TypeRef("value", "#{date}"),
+								CREATION_TIME = new TypeRef("value", "#{time}");
+
 	public TypeRef(String handlerType, String ref) {
 		this.handlerType = handlerType;
 		this.ref = ref;
 	}
-	
+
 	private String handlerType;
 	private String ref;
-	
+
 	String getHandlerType() {
 		return handlerType == null ? CoreConstants.EMPTY : handlerType;
 	}
-	
-	String getRef() {
+
+	public String getRef() {
 		return ref == null ? CoreConstants.EMPTY : ref;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return getHandlerType().hashCode() + getRef().hashCode();
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
-		
+
 		if (!super.equals(obj) && (obj instanceof TypeRef)) {
-			
+
 			return getHandlerType().equals(((TypeRef) obj).getHandlerType())
 			        && getRef().equals(((TypeRef) obj).getRef());
 		}
-		
+
 		return false;
 	}
 }
