95fcabc82a74e95b4a026170ea01cc38bb5cced2
Valdas Žemaitis <valdas@idega.com>
2014-07-03 13:51:10 +0300

    Using constant

--- src/java/com/idega/jbpm/process/business/EndProcessHandler.java
+++ src/java/com/idega/jbpm/process/business/EndProcessHandler.java
@@ -5,25 +5,27 @@ import java.util.List;
 import org.jbpm.graph.def.ActionHandler;
 import org.jbpm.graph.exe.ExecutionContext;
 import org.jbpm.graph.exe.Token;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 /**
  * @deprecated not needed, just add end-complete-process on the end state node
  * TODO: does this end subprocess tokens too?
- * 
+ *
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
  * @version $Revision: 1.5 $
- * 
+ *
  *          Last modified: $Date: 2008/12/03 10:04:54 $ by $Author: civilis $
  */
 @Deprecated
 @Service("endProcessHandler")
-@Scope("prototype")
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class EndProcessHandler implements ActionHandler {
 
 	private static final long serialVersionUID = -4463378796598145701L;
 
+	@Override
 	public void execute(ExecutionContext ctx) throws Exception {
 		ctx.getProcessInstance().end();
 
