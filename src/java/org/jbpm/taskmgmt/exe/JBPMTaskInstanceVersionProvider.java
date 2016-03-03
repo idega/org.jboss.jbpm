c83b2f730393eb31cbf455cf33b69c503847181d
Valdas Žemaitis <valdas@idega.com>
2016-03-03 12:26:34 +0200

    Improvements for jBPM integration (part 2)

--- src/java/org/jbpm/taskmgmt/exe/JBPMTaskInstanceVersionProvider.java
+++ src/java/org/jbpm/taskmgmt/exe/JBPMTaskInstanceVersionProvider.java
@@ -4,15 +4,20 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
-import com.idega.jbpm.version.BPMInstanceVersionProvider;
+import com.idega.jbpm.version.BPMInstanceModificationProvider;
 
 @Service("jbpmTaskInstanceVersionUpdater")
 @Scope(BeanDefinition.SCOPE_SINGLETON)
-public class JBPMTaskInstanceVersionProvider implements BPMInstanceVersionProvider<TaskInstance> {
+public class JBPMTaskInstanceVersionProvider implements BPMInstanceModificationProvider<TaskInstance> {
 
 	@Override
 	public Number getVersion(TaskInstance taskInstance) {
 		return taskInstance == null ? null : taskInstance.version;
+	}
+
+	@Override
+	public void setId(TaskInstance instance, Long id) {
+		instance.id = id;
 	}
 
 }
