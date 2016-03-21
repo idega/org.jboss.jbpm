dee29eb26c8841ff79c10649f259bb24a58e3277
Valdas Žemaitis <valdas@idega.com>
2016-03-21 17:22:19 +0200

    Trying to get citizens group

--- src/java/com/idega/jbpm/identity/authentication/CreateUserHandler.java
+++ src/java/com/idega/jbpm/identity/authentication/CreateUserHandler.java
@@ -34,10 +34,10 @@ import com.idega.util.text.Name;
 /**
  *  Jbpm action handler, which creates ic_user by user personal data object information.
  *  Stores result (ic_user id) to variable provided.
- *   
+ *
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
  * @version $Revision: 1.14 $
- * 
+ *
  * Last modified: $Date: 2009/06/23 15:37:21 $ by $Author: valdas $
  */
 @Service(CreateUserHandler.BEAN_NAME)
@@ -45,42 +45,43 @@ import com.idega.util.text.Name;
 public class CreateUserHandler extends DefaultSpringBean implements ActionHandler {
 
 	public static final String BEAN_NAME = "createUserHandler";
-	
+
 	private static final long serialVersionUID = -1181069105207752204L;
-	
+
 	private UserPersonalData userData;
-	
-	@Autowired(required=false)
+
+	@Autowired(required = false)
 	private StandardGroup standardGroup;
-	
+
 	private boolean publishEvent = Boolean.TRUE;
-	
+
+	@Override
 	public void execute(ExecutionContext ectx) throws Exception {
 		if (getUserData() == null) {
 			getLogger().warning("Called create user handler, but no user data provided. Process instance id="+ectx.getProcessInstance().getId());
 			return;
 		}
-		
+
 		UserPersonalData upd = getUserData();
 		createUser(upd);
 	}
-	
+
 	public User createUser(UserPersonalData upd) throws Exception {
 		if (upd.getUserId() != null) {
 			getLogger().warning("Tried to create user, but UserPersonalData already contained userId="+upd.getPersonalId());
 			return null;
 		}
-		
+
 		String personalId = upd.getPersonalId();
 		if (StringUtil.isEmpty(personalId)) {
 			getLogger().warning("Tried to create user, but no personalId found in UserPersonalData. Skipping.");
 			return null;
 		}
-		
+
 		FacesContext fctx = FacesContext.getCurrentInstance();
 		IWApplicationContext iwac = fctx == null? IWMainApplication.getDefaultIWApplicationContext() : IWMainApplication.getIWMainApplication(fctx).getIWApplicationContext();
 		UserBusiness userBusiness = getUserBusiness(iwac);
-		
+
 		Gender gender = upd.getGender();
 		IWTimestamp dateOfBirth = null;
 		if (upd.getPersonalId() != null && upd.getPersonalId().length() != 0) {
@@ -88,14 +89,14 @@ public class CreateUserHandler extends DefaultSpringBean implements ActionHandle
 			if (dob != null)
 				dateOfBirth = new IWTimestamp(dob);
 		}
-		
+
 		final User usrCreated;
 		if (upd.getCreateWithLogin()) {
 			String firstName;
 			String middleName;
 			String lastName;
 			String userName;
-			
+
 			if (upd.getFullName() != null) {
 				Name name = new Name(upd.getFullName());
 				firstName = name.getFirstName();
@@ -106,32 +107,32 @@ public class CreateUserHandler extends DefaultSpringBean implements ActionHandle
 				middleName = null;
 				lastName = upd.getLastName();
 			}
-			
+
 			if ((userName = upd.getUserName()) == null)
 				userName = upd.getPersonalId();
-				
+
 			String password = LoginDBHandler.getGeneratedPasswordForUser();
 			upd.setUserPassword(password);
-			
+
 			StandardGroup standardGroup = getStandardGroup();
 			final Integer standardGroupPK;
 			if (standardGroup != null)
 				standardGroupPK = new Integer(standardGroup.getGroup().getPrimaryKey().toString());
 			else
 				standardGroupPK = null;
-			
+
 //			doesn't check if login already exists, therefore this check needs to be made before calling this
-			usrCreated = userBusiness.createUserWithLogin(firstName, middleName, lastName, upd.getPersonalId(), null, null, 
+			usrCreated = userBusiness.createUserWithLogin(firstName, middleName, lastName, upd.getPersonalId(), null, null,
 						gender != null ? new Integer(gender.getPrimaryKey().toString()) : null,
 						dateOfBirth, standardGroupPK, userName, password, Boolean.TRUE, IWTimestamp.RightNow(), 5000, Boolean.FALSE, Boolean.TRUE, Boolean.FALSE, null);
 		} else {
 			if (upd.getFullName() != null) {
 				usrCreated = userBusiness.createUserByPersonalIDIfDoesNotExist(upd.getFullName(), personalId, gender, dateOfBirth);
 			} else {
-				usrCreated = userBusiness.createUserByPersonalIDIfDoesNotExist(upd.getFirstName(), null, upd.getLastName(), personalId, gender, dateOfBirth);							
+				usrCreated = userBusiness.createUserByPersonalIDIfDoesNotExist(upd.getFirstName(), null, upd.getLastName(), personalId, gender, dateOfBirth);
 			}
 		}
-		
+
 //		TODO: populated user with other personal data here
 		if (upd.getUserEmail() != null) {
 			EmailHome eHome = userBusiness.getEmailHome();
@@ -140,7 +141,7 @@ public class CreateUserHandler extends DefaultSpringBean implements ActionHandle
 			uEmail.store();
 			usrCreated.addEmail(uEmail);
 		}
-		
+
 		LocateUserHandler.updateAddress(userBusiness, usrCreated, upd);
 		if (upd.getUserPhone() != null)
 			userBusiness.updateUserHomePhone(usrCreated, upd.getUserPhone());
@@ -150,24 +151,24 @@ public class CreateUserHandler extends DefaultSpringBean implements ActionHandle
 //		crappy -no datatype pk- handling
 		final Integer usrId = pk instanceof Integer ? (Integer) pk : new Integer(pk.toString());
 		upd.setUserId(usrId);
-		
+
 		if (isPublishEvent())
 			ELUtil.getInstance().publishEvent(new UserCreatedEvent(this, usrCreated));
-		
+
 		return usrCreated;
 	}
-	
+
 	public void setPublishEvent(boolean publishEvent) {
 		this.publishEvent = publishEvent;
 	}
-	
+
 	public boolean isPublishEvent() {
 		return publishEvent;
 	}
-	
+
 	protected UserBusiness getUserBusiness(IWApplicationContext iwac) {
 		try {
-			return (UserBusiness) IBOLookup.getServiceInstance(iwac, UserBusiness.class);
+			return IBOLookup.getServiceInstance(iwac, UserBusiness.class);
 		}
 		catch (IBOLookupException ile) {
 			throw new IBORuntimeException(ile);
@@ -175,7 +176,9 @@ public class CreateUserHandler extends DefaultSpringBean implements ActionHandle
 	}
 
 	public StandardGroup getStandardGroup() {
-		
+		if (standardGroup == null) {
+			standardGroup = ELUtil.getInstance().getBean("citizenStandardGroup");
+		}
 		return standardGroup;
 	}
 
