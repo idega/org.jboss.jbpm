6860099c2649364164b06c5617b7272a11c642c0
Valdas Žemaitis <valdas@idega.com>
2022-09-12 11:03:33 +0300

    Using Long

--- src/java/com/idega/jbpm/event/ProcessInstanceCreatedEvent.java
+++ src/java/com/idega/jbpm/event/ProcessInstanceCreatedEvent.java
@@ -1,6 +1,5 @@
 package com.idega.jbpm.event;
 
-import java.io.Serializable;
 import java.util.Map;
 
 import org.springframework.context.ApplicationEvent;
@@ -10,7 +9,7 @@ public class ProcessInstanceCreatedEvent extends ApplicationEvent {
 	private static final long serialVersionUID = 8587349554332579987L;
 
 	private String processDefinitionName;
-	private Serializable processDefintionId, processInstanceId;
+	private Long processDefintionId, processInstanceId;
 	private Map<String, Object> variables;
 
 	public ProcessInstanceCreatedEvent(String processDefinitionName, Long processInstanceId, Map<String, Object> variables) {
@@ -25,16 +24,12 @@ public class ProcessInstanceCreatedEvent extends ApplicationEvent {
 		return processDefinitionName;
 	}
 
-	public <T extends Serializable> T getProcessDefintionId() {
-		@SuppressWarnings("unchecked")
-		T result = (T) processDefintionId;
-		return result;
+	public Long getProcessDefintionId() {
+		return processDefintionId;
 	}
 
-	public <T extends Serializable> T getProcessInstanceId() {
-		@SuppressWarnings("unchecked")
-		T result = (T) processInstanceId;
-		return result;
+	public Long getProcessInstanceId() {
+		return processInstanceId;
 	}
 
 	public Map<String, Object> getVariables() {
